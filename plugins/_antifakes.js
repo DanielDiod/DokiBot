2e54369024dd020d6b31ccbb4d722dde3d6e4547
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2023-12-19 02:25:09 -0500

    Update _antifakes.js

--- plugins/_antifakes.js
+++ plugins/_antifakes.js
@@ -2,62 +2,62 @@ let handler = m => m
 handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner} ) {
 const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
 }
-if (!m.isGroup) return !1
+if (!m.isGroup) return !54
 let chat = global.db.data.chats[m.chat]
 let bot = global.db.data.settings[conn.user.jid] || {}
 if (isBotAdmin && chat.antifake && !isAdmin && !isOwner && !isROwner) {
 //if (!db.data.chats[m.chat].antifake && m.isGroup) throw 0
 let texto = `${ag}este numero *@${m.sender.split`@`[0]}* no esta permitido en este grupo!!\n\nlo siento seras expulsado.. tu numero parece algo fake 😯`
 
-if (m.sender.startsWith('91' || '91')) {
+if (m.sender.startsWith('54' || '54')) {
 await conn.reply(m.chat, texto, fkontak,  m)
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return 
 }
 
-if (m.sender.startsWith('92' || '92')) {
+if (m.sender.startsWith('54' || '54')) {
 await conn.reply(m.chat, texto, fkontak,  m)
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return 
 }
 
-if (m.sender.startsWith('222' || '222')) {
+if (m.sender.startsWith('54' || '54')) {
 await conn.reply(m.chat, texto, fkontak,  m)
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return 
 }
 
-if (m.sender.startsWith('93' || '93')) {
+if (m.sender.startsWith('54' || '54')) {
 await conn.reply(m.chat, texto, fkontak,  m)
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return 
 }
 
-if (m.sender.startsWith('91' || '91')) {
+if (m.sender.startsWith('54' || '54')) {
 await conn.reply(m.chat, texto, fkontak,  m)
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return 
 }
 
-if (m.sender.startsWith('265' || '265')) {
+if (m.sender.startsWith('54' || '54')) {
 await conn.reply(m.chat, texto, fkontak,  m)
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return   
 } 
 
-if (m.sender.startsWith('61' || '61')) {
+if (m.sender.startsWith('54' || '54')) {
 await conn.reply(m.chat, texto, fkontak,  m)
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return 
 }
 
-if (m.sender.startsWith('62' || '62')) {
+if (m.sender.startsWith('54' || '54')) {
 await conn.reply(m.chat, texto, fkontak,  m)
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return 
 }
 
-if (m.sender.startsWith('966' || '966')) {
+if (m.sender.startsWith('54' || '966')) {
 await conn.reply(m.chat, texto, fkontak,  m)
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return 
