672d6dd7631713ea1a1ba2eeb2e2ac44a3f30639
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2023-12-22 02:08:14 -0500

    Update propietario(a)-update.js

--- plugins/propietario(a)-update.js
+++ plugins/propietario(a)-update.js
@@ -5,7 +5,7 @@ if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
 conn.reply(m.chat, stdout.toString(), m)}
 //} catch {
-var update = execSync('git remote set-url origin https://github.com/elrebelde21/The-LoliBot-MD.git && git pull')
+var update = execSync('git remote set-url origin https://github.com/DanielDiod/DokiBot.git && git pull')
 await m.reply(update.toString())
 } catch {
 await m.reply(`${fg}`) 
