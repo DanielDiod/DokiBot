e895c1fd7c5e3c3e959e3380878111fa0058b3ee
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2023-12-22 18:28:05 -0500

    Update aviso-owner.js

--- plugins/aviso-owner.js
+++ plugins/aviso-owner.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, command, paypal }) => {
-conn.sendMessage(m.chat, {text:'*AVISO: Yo "Daniel", he tomado la decisión de retirarme de todo lo relacionado con el bot o temas similares,por el motivó de algunos problema familiares y que por ahora no me encuentro apto para seguir continuando esto quiere decir, que en algún momento quedará obsoleto y/o algunos comandos dejarán de funcionar*'},  { quoted: m })
+conn.sendMessage(m.chat, {text:'*AVISO: Yo "Daniel", he tomado la decisión de retirarme de todo lo relacionado con el bot o temas similares,por el motivó de algunos problema familiares y que por ahora no me encuentro apto para seguir continuando esto quiere decir, que en algún momento quedará obsoleto y/o algunos comandos dejarán de funcionar, Agradezco su preferencia durante el recorrido por este mundo de los bots.*\n\n*MonkiBot pasa a quedar en manos de colaboradores, y como principal representante, @sopiix*\n\n*agradecer a amigos que siempre estuvieron apoyando a este lindo proyecto , pero es hora de decir adios 👋🏼*'},  { quoted: m })
 }
 handler.help = ['pagina']
 handler.tags = ['info']
