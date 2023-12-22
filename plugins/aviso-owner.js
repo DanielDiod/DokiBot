3b1efcffbbf55f1721f75f451b80ce91ed1015d8
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2023-12-22 18:22:59 -0500

    Update aviso-owner.js

--- plugins/aviso-owner.js
+++ plugins/aviso-owner.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, command, paypal }) => {
-conn.sendMessage(m.chat, {text:'*AVISO: Yo "Daniel", he tomado la decisión de retirarme de todo lo relacionado con el bot o temas similares,*'},  { quoted: m })
+conn.sendMessage(m.chat, {text:'*AVISO: Yo "Daniel", he tomado la decisión de retirarme de todo lo relacionado con el bot o temas similares,por el motivó de algunos problema familiares y que por ahora no me encuentro apto para seguir continuando esto quiere decir*'},  { quoted: m })
 }
 handler.help = ['pagina']
 handler.tags = ['info']
