4cc469a1711d6f7c68708123393dcb3254b3883b
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2023-12-26 23:51:06 -0500

    Update menu-bot.js

--- plugins/menu-bot.js
+++ plugins/menu-bot.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, usedPrefix, command}) => {
 
 let Reglas = ` *🛑INFORMACIÓN DEL MENU🛑*\n
 📝𝗡𝗼𝘁𝗮 :𝐡𝐨𝐥𝐚 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 𝐡𝐚𝐬 𝐬𝐨𝐥𝐢𝐜𝐢𝐭𝐚𝐝𝐨 𝐦𝐢 𝐦𝐞𝐧𝐮 , 𝐬𝐢 𝐝𝐞𝐬𝐞𝐚𝐬 𝐯𝐞𝐫 𝐦𝐢 𝐦𝐞𝐧𝐮 𝐦𝐚𝐧𝐝𝐚𝐫 𝐞𝐥 𝐬𝐢𝐠𝐮𝐢𝐞𝐧𝐭𝐞 𝐜𝐨𝐦𝐚𝐧𝐝𝐨 :
-.*menucompleto*\n
+.*herramientas*\n
 *se te agradecería mucho si te unes al canal de MonkiBot 👇🏼*\n
 ${md}
 `.trim()
