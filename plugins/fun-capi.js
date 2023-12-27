4bf6545774db407c5e04836b7c54de3f4466a712
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2023-12-26 23:28:01 -0500

    Create fun-capi.js

---
+++ plugins/fun-capi.js
@@ -0,0 +1,19 @@
+var handler = async (m, { conn, command, text }) => {
+
+if (!text) throw `*⚠️ ESCRIBE EL NOMBRE DE DOS PERSONAS PARA CALCULAR SU AMOR*`
+let [text1, ...text2] = text.split(' ')
+
+text2 = (text2 || []).join(' ')
+if (!text2) throw `*⚠️ ESCRIBE EL NOMBRE DE LA SEGUNDA PERSONA*`
+let love = `❤️ *${text1}* tu oportunidad de enamorarte de *${text2}* es de *${Math.floor(Math.random() * 100)}%*👩🏻‍❤️‍👨🏻 
+
+_*by Gael Dios*_
+`.trim()
+m.reply(love, null, { mentions: conn.parseMention(love) })
+
+}
+handler.help = ['love']
+handler.tags = ['fun']
+handler.command = /^(ship)$/i
+
+export default handler
