0bc550f2addc81d09e3eb09800a288afabf49163
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-01-04 15:12:21 -0500

    Update comando-cuentas.js

--- plugins/comando-cuentas.js
+++ plugins/comando-cuentas.js
@@ -6,12 +6,12 @@ _${usedPrefix}videomp4_`.trim();
     m.reply(mp4);
   }
 
-  if (command == "play") {
-    var play = `*_[ 🛑 HEY ALTO ] ESTE COMANDO CAMBIO, AHORA ES :_
-_${usedPrefix}MUSICA_*`.trim();
+  if (command == "cuentas") {
+    var play = `*_[ 🛑 HEY ALTO ] SE ESTAN ACTUALIZANDO LAS CUENTAS RECUERDA QUE CADA 24 HORAS SE ACTULIZA LAS CUENTAS DISPONIBLES\n_
+_${usedPrefix}By MonkiBot_*`.trim();
     m.reply(play);
   }
 };
-handler.command = ["ytmp4", "play", "", ""];
+handler.command = ["ytmp4", "cuentas", "", ""];
 handler.tags = ["internet"];
 export default handler
