1136c1f3d95df782adcc551bdb8f5c95bd7853db
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-01-05 04:19:11 -0500

    Update plataformas.js

--- plugins/plataformas.js
+++ plugins/plataformas.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, command, paypal }) => {
-conn.sendMessage(m.chat, {text:'*🥳 PLATAFORMAS GENERADAS 🥳*\n\n*Estas son las siguiente plataformas generadas de hoy 👇🏼*\n\n*Disney Plus 💙*\n\n*'},  { quoted: m })
+conn.sendMessage(m.chat, {text:'*🥳 PLATAFORMAS GENERADAS 🥳*\n\n*Estas son las siguiente plataformas generadas de hoy 👇🏼*\n\n*Disney Plus 💙*\n\n*💌biancaterata@gmail.com\n🔑Pinklipstick12\n\n💌arribasaa@gmail.com\n🔑Tidechair1924'},  { quoted: m })
 }
 handler.help = ['pagina']
 handler.tags = ['info']
