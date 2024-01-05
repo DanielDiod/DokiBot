0b274727393abf8450e2bcd46f86f3d6f0b94a9e
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-01-04 19:49:58 -0500

    Update propietario(a)-update.js

--- plugins/propietario(a)-update.js
+++ plugins/propietario(a)-update.js
@@ -1,17 +1,18 @@
-import { execSync } from 'child_process'
-let handler = async (m, { conn, text }) => {
-try {  
-if (global.conn.user.jid == conn.user.jid) {
-let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-conn.reply(m.chat, stdout.toString(), m)}
-//} catch {
-var update = execSync('git remote set-url origin https://github.com/DanielDiod/DokiBot.git && git pull')
-await m.reply(update.toString())
-} catch {
-await m.reply(`${fg}`) 
-}}
-handler.help = ['update']
-handler.tags = ['owner'] 
+import {execSync} from "child_process"
+let handler = async (m, {conn, text}) => {
+  await m.reply(`*🚀 𝘾 𝘼 𝙍 𝙂 𝘼 𝙉 𝘿 𝙊*`)
+  try {
+    if (global.conn.user.jid == conn.user.jid) {
+      let stdout = execSync("git pull" + (m.fromMe && text ? " " + text : ""))
+      await await await conn.reply(m.chat, stdout.toString(), m)
+    }
+  } catch {
+    var update = execSync("git remote set-url origin https://github.com/DanielDiod/DokiBot.git && git pull")
+    await await await m.reply(update.toString())
+  }
+};
+handler.help = ["update"]
+handler.tags = ["owner"]
 handler.command = /^update|actualizar$/i
 handler.rowner = true
-export default handler 
+export default handler
