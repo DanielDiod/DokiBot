832ebf3d072d8cf9d9658893e12e95016efe7b9d
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-01-09 11:03:31 -0500

    Update plataformas.js

--- plugins/plataformas.js
+++ plugins/plataformas.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, command, paypal }) => {
-conn.sendMessage(m.chat, {text:'*PLATAFORMAS GENERADAS ✅*\n\n*Estas son las siguiente plataformas generadas de hoy 👇🏼*\n\n*CANVA PRO 🖥️*\n\n💌https://www.canva.com/brand/join?token=DGDare_uZD1QkiKpg6qzRw&referrer=team-invite\n\n*AMAZON MUSIC 3 MESES 🎧*\n\nRecomiendo usar vpn con direccion Brasil 🇧🇷\n\n💌https://a.co/afTOnM8\n\n*HBO MAX 💜*\n\n💌gabuch.gt550@gmail.com\n🔑Dracarysxt85\n\n*BY MONKIBOT 🐒*'},  { quoted: m })
+conn.sendMessage(m.chat, {text:'*PLATAFORMAS GENERADAS ✅*\n\n*Estas son las siguiente plataformas generadas de hoy 👇🏼*\n\n*CANVA PRO 🖥️*\n\n💌https://www.canva.com/brand/join?token=DGDare_uZD1QkiKpg6qzRw&referrer=team-invite\n\n*AMAZON MUSIC 3 MESES 🎧*\n\nRecomiendo usar vpn con direccion Brasil 🇧🇷\n\n💌https://a.co/afTOnM8\n\n*HBO MAX 💜*\n\n💌gabuch.gt550@gmail.com\n🔑Dracarysxt85\n\n*AGRADECER AL CREADOR EN EL GRUPO OFICIAL DEL BOT 👇🏼*\n\nhttps://chat.whatsapp.com/GbrlQFMXcMb39vOpNaNsZ2\n\nBY MONKIBOT 🐒*'},  { quoted: m })
 }
 handler.help = ['pagina']
 handler.tags = ['info']
