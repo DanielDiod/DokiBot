c2dd42a8b761943184bb1b5c45b634fe0838631e
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-01-15 17:39:41 -0500

    Update descarga-prueba.js

--- plugins/descarga-prueba.js
+++ plugins/descarga-prueba.js
@@ -1,25 +1,23 @@
 import fetch from 'node-fetch'
 import fs from 'fs'
+let handler = async (m, {text, usedPrefix, command, conn}) => {
+if (!text) throw `*⚠️ Ingrese el nombre de la apk o app que desea buscar.*`
 
-let handler  = async (m, { conn, usedPrefix: prefix, command, text }) => {
-//try {
-if (!text) throw `*⚠️ Ingrese el nombre de la apk o app que desea buscar.*\n_📌 Ejemplo:_ *${ prefix + command}* WhatsApp`
-let sfoto = fs.readFileSync('./storage/image/apksearch.png')
-
-let json = await fetch(`https://api.akuari.my.id/search/searchapk2?query=${text}`)
+let img = await (await fetch('https://telegra.ph/file/2df89bd87eae9e3173635.jpg')).buffer()
+let json = await fetch(`https://api.lolhuman.xyz/api/groupwhatsapp?apikey=${lolkeysapi}&query=${text}`)
 let jsons = await json.json()
-let res = Object.values(jsons.respon).map(v => v)
+let res = Object.values(jsons.result).map(v => v)
 if (!res.length) return m.reply('⚠️ *_No se encontraron resultados, intente con otro nombre._*').then(_ => m.react('✖️'))
 let txt = `*⊜ RESULTADOS🔎*`
 for (let i = 0; i < (15 <= res.length ? 15 : res.length); i++) {
-   txt += `\n`
+   txt += `\n\n`
    txt += `	⤿  *Nro* : ${1+i}\n`
    txt += `	⤿  *Nombre* : ${res[i].title}\n`
    txt += `	⤿  *Link* : ${res[i].link}\n`
-   txt += `	⤿  *Actualizado* : ${res[i].update}\n`
 }
-conn.sendNyanCat(m.chat, txt, sfoto, botname, null, script, m)
-
-}    
-handler.command = ['searchapk', 'apksearch']
+conn.sendNyanCat(m.chat, txt, img, botname, null, script, m)
+}
+handler.tags = ['apk']
+handler.command = handler.help = ['apk']
+handler.register = true
 export default handler
