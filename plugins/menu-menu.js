7d7cc9e7c33c7fc8b642ea2bcd806f3a35173656
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-01-15 23:24:28 -0500

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -71,7 +71,11 @@ let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 //let enlace = { contextInfo: { externalAdReply: {title: wm, body: 'support group' , sourceUrl: nna, thumbnail: await(await fetch(img)).buffer() }}}
   let pp = './Menu2.jpg'
 //let pp = gataVidMenu.getRandom()
-await m.reply(`ENVIANDO EL MENU..`);	
+await m.reply(`🙈 𝙀𝙣𝙫𝙞𝙖𝙣𝙙𝙤 𝙚𝙡 𝙢𝙚𝙣𝙪 𝙙𝙚 𝙈𝙤𝙣𝙠𝙞𝘽𝙤𝙩...
+
+ʚ 𝗘𝘀𝗽𝗲𝗿𝗲 𝘂𝗻 𝗺𝗼𝗺𝗲𝗻𝘁𝗼 𝗽𝗼𝗿 𝗳𝗮𝘃𝗼𝗿 ɞ
+
+» 𝗦𝗶 𝗻𝗼 𝗲𝘀𝗽𝗲𝗿𝗮 𝗲𝘀 𝗽𝗼𝗿𝗾𝘂𝗲 𝘀𝘂 𝘁𝗶𝗼 𝗻𝗼 𝗹𝗲 𝗱𝗶𝗼 𝗮𝗻𝗼𝗰𝗵𝗲 👀`);	
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let fsizedoc = '1'.repeat(10)
 let adReply = { fileLength: fsizedoc, seconds: fsizedoc, contextInfo: { forwardingScore: fsizedoc, externalAdReply: { showAdAttribution: true, title: wm, body: '👋 ' + username, mediaUrl: ig, description: 'Hola', previewType: 'PHOTO', thumbnail: await(await fetch(global.img)).buffer(), sourceUrl: redesMenu.getRandom() }}}
