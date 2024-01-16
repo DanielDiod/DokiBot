e5a5c7ec07fbd9cc010d798212af9ac0c3ffaef5
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-01-16 00:54:51 -0500

    Update fun-pepon.js

--- plugins/fun-pepon.js
+++ plugins/fun-pepon.js
@@ -1,12 +1,1 @@
-import fs from "fs" 
- let handler = m => m 
- handler.all = async function (m) { 
- let bot = `Codigo gay` 
- let vn = [pepon1, pepon2, img1].getRandom() 
- let chat = global.db.data.chats[m.chat] 
- if (/^sn$/i.test(m.text) && !chat.isBanned) {  
- conn.sendPresenceUpdate('recording', m.chat)     
- conn.sendFile(m.chat, vn, 'bot.jpg', bot, m, true, { type: 'conversation', ptt: true, sendEphemeral: true, quoted: estilo })} 
- return !0 
- } 
- export default handler
+pene
