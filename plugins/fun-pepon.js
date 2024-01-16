8c2621f887e3f1ba0fd41189e5005618ec50f866
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-01-16 00:51:06 -0500

    Update fun-pepon.js

--- plugins/fun-pepon.js
+++ plugins/fun-pepon.js
@@ -2,7 +2,7 @@ import fs from "fs"
  let handler = m => m 
  handler.all = async function (m) { 
  let bot = `*🚀 Hola soy Bot*\nEn que te puedo ayudar?` 
- let vn = [imagen1, imagen2, imagen3, imagen4, img1].getRandom() 
+ let vn = [pepon1, pepon2, img1].getRandom() 
  let chat = global.db.data.chats[m.chat] 
  if (/^bot$/i.test(m.text) && !chat.isBanned) {  
  conn.sendPresenceUpdate('recording', m.chat)     
