905a760f96031d672477dfe8e3a5ae663bc32847
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-01-19 19:35:38 -0500

    Update rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -1,15 +1,15 @@
 import PhoneNumber from 'awesome-phonenumber' 
-import fetch from 'node-fetch' 
-let handler = async (m, { conn }) => { 
-let _pp = 'https://telegra.ph/file/9ce4d19dfbe3d4744cd89.jpg' 
-let user = db.data.users[m.sender] 
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender 
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png') 
-let { premium, level, diamond, exp, lastclaim, registered, regTime, age } = global.db.data.users[m.sender] 
-let username = conn.getName(who) 
-let name = conn.getName(who) 
-let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}} 
-let str = ` 
+ import fetch from 'node-fetch' 
+ let handler = async (m, { conn }) => { 
+   let _pp = './storage/logos/Bailey.jpg' 
+   let user = db.data.users[m.sender] 
+   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender 
+     let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png') 
+     let { premium, level, diamond, exp, lastclaim, registered, regTime, age } = global.db.data.users[m.sender] 
+     let username = conn.getName(who) 
+     let name = conn.getName(who) 
+     let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}} 
+     let str = ` 
  . . . *🔰 P E R F I L 🔰* . . . 
  💌 • *Nombre:* ${username}  
  📧 • *Tag:* @${who.replace(/@.+/, '')} 
@@ -20,7 +20,7 @@ let str = `
  📑 • *Registrado:* ${registered ? '✅': '❌'} 
  🌟 • *Premium:* ${premium ? "✅\n⏰ • *PremiumTime:* ${clockString(user.premiumTime)}" :"❌"} 
  `.trim() 
-  m.react(🌟)  
+   //m.react(🌟)  
   conn.sendFile(m.chat, pp, 'perfil.jpg', str, fkon, false, { mentions: [who] }) 
  } 
  handler.help = ['profile [@user]'] 
@@ -32,9 +32,9 @@ let str = `
  const readMore = more.repeat(4001) 
   
  function clockString(ms) { 
- let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000) 
- let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24 
- let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60 
- let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60 
- return [d, ' *Dias ☀️*\n ', h, ' *Horas 🕐*\n ', m, ' *Minutos ⏰*\n ', s, ' *Segundos ⏱️* '].map(v => v.toString().padStart(2, 0)).join('') 
- }
+   let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000) 
+   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24 
+   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60 
+   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60 
+   return [d, ' *Dias ☀️*\n ', h, ' *Horas 🕐*\n ', m, ' *Minutos ⏰*\n ', s, ' *Segundos ⏱️* '].map(v => v.toString().padStart(2, 0)).join('') 
+          
