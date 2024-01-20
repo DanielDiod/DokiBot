67e071c571b6d15ec5c78ff17077a9ff12a36a1e
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-01-19 20:13:50 -0500

    Update rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -10,18 +10,18 @@ import PhoneNumber from 'awesome-phonenumber'
      let name = conn.getName(who) 
      let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}} 
      let str = ` 
- ┏━━❬ *🔰 P E R F I L 🔰*  ❭━━┓ \n\n
+ ┏━━❬ *🔰 P E R F I L 🔰*  ❭━━┓ \n
  💌 • *Nombre:* ${username}\n\n
- 📧 • *Tag:* @${who.replace(/@.+/, '')}\n\n 
- 📞 • *Numero:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}\n\n 
- 🔗 • *Link:* https://wa.me/${who.split`@`[0]}\n\n 
- 🎨 • *Edad:* ${registered ? age : ''}\n\n 
- 💎 • *Limite: ${diamond} Usos*\n\n  
+ 📧 • *Tag:* @${who.replace(/@.+/, '')}\n
+ 📞 • *Numero:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}\n 
+ 🔗 • *Link:* https://wa.me/${who.split`@`[0]}\n 
+ 🎨 • *Edad:* ${registered ? age : ''}\n 
+ 💎 • *Limite: ${diamond} Usos*\n  
  📑 • *Registrado:* ${registered ? '✅': '❌'}\n\n 
- 🌟 • *Premium:* ${premium ? "✅\n⏰ • *PremiumTime:* ${clockString(user.premiumTime)}" :"❌"}\n\n
+ 🌟 • *Premium:* ${premium ? "✅\n⏰ • *PremiumTime:* ${clockString(user.premiumTime)}" :"❌"}\n
  ┗━━━━━━━━━━━━━━
  `.trim() 
-   //m.react(🌟)  
+  await m.react('⭐')
   conn.sendFile(m.chat, pp, 'perfil.jpg', str, fkon, false, { mentions: [who] }) 
  } 
  handler.help = ['profile [@user]'] 
