3bee320cec0581966153bddef2e04e10f639d343
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-01-21 10:56:09 -0500

    Update rpg-shops_Tienda.js

--- plugins/rpg-shops_Tienda.js
+++ plugins/rpg-shops_Tienda.js
@@ -1,39 +1,22 @@
+const xpperlimit = 350;
+const handler = async (m, {conn, command, args}) => {
+  let count = command.replace(/^buy/i, '');
+  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
+  count = Math.max(1, count);
+  if (global.db.data.users[m.sender].exp >= xpperlimit * count) {
+    global.db.data.users[m.sender].exp -= xpperlimit * count;
+    global.db.data.users[m.sender].limit += count;
+    conn.reply(m.chat, `
+┌─「 *NOTA DE PAGO* 」
+‣ *Compra nominal* : + ${count}💎 
+‣ *Gastado* : -${xpperlimit * count} XP
+└──────────────`, m);
+  } else conn.reply(m.chat, `❎ Lo siento, no tienes suficientes *XP* para comprar *${count}* Diamantes💎`, m);
+};
+handler.help = ['Buy', 'Buyall'];
+handler.tags = ['xp'];
+handler.command = ['buy', 'buyall'];
 
-const precioDiamante = 200 
-let handler = async (m, { conn, usedPrefix, command, args }) => {
+handler.disabled = false;
 
-  let user = global.db.data.users[m.sender]
- 
-  if (!args[0]) throw `📌 ${mssg.example}: *${usedPrefix + command}* all\n*${usedPrefix + command}* 8`;
-  if (args[0].toLowerCase() !== 'all' && !/^[1-9]\d*$/.test(args[0])) throw `✳️ ${mssg.isNan}`;
-
-  let all =  Math.floor(user.coin / precioDiamante)
- let count = args[0].replace('all', all)
- count = Math.max(1, count)
-  //if (isNaN(count)) throw `✳️ ${mssg.isNan}`;
-
-  
-  let totalCost = precioDiamante * count;
-
-  if (user.coin >= totalCost) {
-    user.coin -= totalCost;
-    user.diamond += count;
-
-    m.reply(`
-┌─「 *${mssg.voucher.toUpperCase()}* 」
-‣ *${mssg.buy}:* ${mssg.dmd}
-‣ *${mssg.buyCount}:* ${count.toLocaleString()} 💎 
-‣ *${mssg.spent}:* -${totalCost.toLocaleString()} 🪙
-└──────────────`);
-  } else {
-    m.reply(`❎ ${mssg.buyNan('Coins')} *${count}* 💎`);
-  }
-
-}
-handler.help = ['buy']
-handler.tags = ['econ']
-handler.command = ['buy'] 
-
-handler.disabled = false
-
-export default handler
+export default handler;
