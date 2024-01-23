463dcb1d6abefb8911e859a0bcc212ca3b9516d8
Dali-13 <157506744+Dali-13@users.noreply.github.com>
2024-01-23 15:39:20 -0500

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -17,10 +17,11 @@ if (/webp|image|video/g.test(mime)) {
 let img = await q.download?.()
 let out
 stiker = await sticker(img, false, global.packname, global.author)
-await conn.reply(m.chat, `𝗛𝗲𝘆! 𝘁𝘂 𝘀𝘁𝗶𝗰𝗸𝗲𝗿 𝘀𝗲 𝗲𝘀𝘁𝗮 𝗰𝗿𝗲𝗮𝗻𝗱𝗼
-𝘁𝗲𝗻 𝗽𝗮𝗰𝗶𝗲𝗻𝗰𝗶𝗮 𝘆 𝗲𝘃𝗶𝘁𝗮 𝗲𝗹 𝘀𝗽𝗮𝗺 👀
+await conn.reply(m.chat, `‼️ 𝖢𝖠𝖫𝖬𝖠 𝖡𝖱𝖮 ‼️
 
-𝐁𝐲 𝐌𝐨𝐧𝐤𝐢𝐁𝐨𝐭🐒`, m)
+🤖🔧 𝖣𝖺𝗆𝖾 𝗎𝗇 𝗆𝗈𝗆𝖾𝗇𝗍𝗈, 𝖾𝗌𝗍𝗈𝗒 𝖼𝗋𝖾𝖺𝗇𝖽𝗈 𝗍𝗎 𝗌𝗍𝗂𝖼𝗄𝖾𝗋…
+
+☁️ 𝖤𝗏𝗂𝗍𝖺 𝖾𝗅 𝗌𝗉𝖺𝗆 ✋🏻`, m)
 
 if (!stiker) {
 if (/webp/g.test(mime)) out = await webp2png(img)
