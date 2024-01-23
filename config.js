b1dcdf2c1048709cd0eb11556de346e8f986c131
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-01-23 03:58:20 -0500

    Update config.js

--- config.js
+++ config.js
@@ -146,9 +146,9 @@ global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.pre
 
 //⊱ ━━━━━.⋅ IMG ⋅.━━━━ ⊰
 
-global.imagen1 = fs.readFileSync('./media/menus/Menu3.jpeg')
-global.imagen2 = fs.readFileSync('./media/menus/Menu1.jpeg')
-global.menu2 = fs.readFileSync('./media/menus/Menu2.jpeg')
+global.imagen1 = fs.readFileSync('./media/menus/Menu3.jpg')
+global.imagen2 = fs.readFileSync('./media/menus/Menu1.jpg')
+global.imagen3 = fs.readFileSync('./media/menus/Menu2.jpg')
 global.img = 'https://i.imgur.com/H6AofpJl.jpg'
 global.img2 = 'https://i.imgur.com/R1uVUId.jpeg'
 
