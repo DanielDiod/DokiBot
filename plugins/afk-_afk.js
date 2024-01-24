dd6160b2deb91698e5a0bbdfd1092ba9467d0da3
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-01-24 18:15:40 -0500

    Update afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -1,26 +1,20 @@
-export function before(m) {
-let user = global.db.data.users[m.sender]
-if (user.afk > -1) {
-m.reply(`
-${lenguajeGB['smsAfkM1']()}${user.afkReason ? ` ${lenguajeGB['smsAfkM2']()} ` + user.afkReason : ''}
-  
-${lenguajeGB['smsAfkM3']()} *${(new Date - user.afk).toTimeString()}*`.trim())
-user.afk = -1
-user.afkReason = ''
-}
-let jids = [...new Set([...(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])])]
-for (let jid of jids) {
-let user = global.db.data.users[jid]
-if (!user)
-continue
-let afkTime = user.afk 
-if (!afkTime || afkTime < 0)
-continue
-let reason = user.afkReason || ''
-m.reply(`${lenguajeGB['smsAfkM4']()}     
-${reason ? `${lenguajeGB['smsAfkM5']()} ` + reason : `${lenguajeGB['smsAfkM6']()}`}
-${lenguajeGB['smsAfkM3']()} *${(new Date - afkTime).toTimeString()}*
-`.trim())
-}
-return true
+export async function before(m) {
+    let user = global.db.data.users[m.sender]
+    if (user.afk > 0) {
+        await conn.reply(m.chat, `🚩 @${m.sender.split`@` [0]} dejaste de estar AFK despues de *${(new Date - user.afk).toTimeString()}*.`, m)
+        user.afk = 0
+        user.afkReason = ''
+    }
+    let jids = [...new Set([...(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])])]
+    for (let jid of jids) {
+        let user = global.db.data.users[jid]
+        if (!user)
+            continue
+        let afkTime = user.afk
+        if (!afkTime || afkTime < 0)
+            continue
+        let reason = user.afkReason || ''
+        await conn.reply(m.chat, `🚩 El usuario que intentas etiquetar esta *AFK* por la razón *${reason ? reason : '...'}* durante *${(new Date - afkTime).toTimeString()}*.`, m)
+    }
+    return true
 }
