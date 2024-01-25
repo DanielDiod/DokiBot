5e05bdddcf426d3a2889687ded9cf00f1815d4da
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-01-24 22:07:07 -0500

    Update rpg-crime.js

--- plugins/rpg-crime.js
+++ plugins/rpg-crime.js
@@ -1,19 +1,19 @@
 //CÓDIGO CREADO POR elrebelde21 : https://github.com/elrebelde21
 const handler = async (m, {conn, isPrems}) => {
 const date = global.db.data.users[m.sender].crime + 3600000; //3600000 = 1 hs
-if (new Date - global.db.data.users[m.sender].crime < 3600000) return m.reply(`『🚓︎』𝙇𝘼 𝙋𝙊𝙇𝙄𝘾𝙄𝘼 𝙀𝙎𝙏𝘼 𝙑𝙄𝙂𝙄𝙇𝘼𝙉𝘿𝙊 𝙀𝙉 𝙀𝙎𝙏𝙀 𝙈𝙊𝙈𝙀𝙉𝙏𝙊, 𝙑𝙐𝙀𝙇𝙑𝙀 𝙀𝙉 : ${msToTime(date - new Date())}`)
+if (new Date - global.db.data.users[m.sender].crime < 3600000) return m.reply(`*『⚠️』LA POLICÍA TE TIENE VIGILADO EN ESTE MOMENTO, VUELVE EN* : ${msToTime(date - new Date())}`)
 const exp = Math.floor(Math.random() * 9000)
 const diamond = Math.floor(Math.random() * 150)
 const money = Math.floor(Math.random() * 9000)
 if (global.db.data.users[m.sender].exp < 0) return m.reply(`〔🪙〕${pickRandom(global.robar)} ${exp} XP`).catch(global.db.data.users[m.sender].exp += exp)
 if (global.db.data.users[m.sender].limit < 0) return m.reply(`〔🪙〕${pickRandom(global.robar)} ${diamond} 💎 Diamante`).catch(global.db.data.users[m.sender].limit += diamond)
-if (global.db.data.users[m.sender].money < 0) return m.reply(`〔🪙〕${pickRandom(global.robar)} ${money} 🐈GataCoins`).catch(global.db.data.users[m.sender].money += money) 
+if (global.db.data.users[m.sender].money < 0) return m.reply(`〔🪙〕${pickRandom(global.robar)} ${money} 🪙ShanaCoins`).catch(global.db.data.users[m.sender].money += money) 
 let or = ['text', 'text2', 'text3', 'text4']; 
 let media = or[Math.floor(Math.random() * 4)]
 global.db.data.users[m.sender].crime = new Date * 1;
-if (media === 'text') return m.reply(`〔💰〕${pickRandom(global.robar)} ${exp} XP`).catch(global.db.data.users[m.sender].exp += exp) 
+if (media === 'text') return m.reply(`〔🪙〕${pickRandom(global.robar)} ${exp} XP`).catch(global.db.data.users[m.sender].exp += exp) 
 if (media === 'text2') return m.reply(`〔🚓〕${pickRandom(global.robmal)} ${exp} XP`).catch(global.db.data.users[m.sender].exp -= exp) 
-if (media === 'text3') return m.reply(`〔💰〕*${pickRandom(global.robar)}*\n\n${diamond} 💎 𝐃𝐈𝐀𝐌𝐀𝐍𝐓𝐄\n${money} 🐈 𝐆𝐀𝐓𝐀𝐂𝐎𝐈𝐍𝐒`).catch(global.db.data.users[m.sender].limit += diamond).catch(global.db.data.users[m.sender].money += money)
+if (media === 'text3') return m.reply(`〔🪙〕*${pickRandom(global.robar)}*\n\n${diamond} 💎 𝐃𝐈𝐀𝐌𝐀𝐍𝐓𝐄\n${money} 🐈 𝐆𝐀𝐓𝐀𝐂𝐎𝐈𝐍𝐒`).catch(global.db.data.users[m.sender].limit += diamond).catch(global.db.data.users[m.sender].money += money)
 if (media === 'text4') return m.reply(`〔🚓〕${pickRandom(global.robmal)}\n\n${diamond} 💎 𝐃𝐈𝐀𝐌𝐀𝐍𝐓𝐄\n${money} 🐈 𝐆𝐀𝐓𝐀𝐂𝐎𝐈𝐍𝐒`).catch(global.db.data.users[m.sender].limit -= diamond).catch(global.db.data.users[m.sender].money -= money) 
 }
 handler.help = ['robar'];
@@ -36,5 +36,5 @@ function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())];
 }
 
-global.robar = ['Robaste un Banco 🏦 y obtuviste', 'Negociarte con el jefe de la mafia y obtuvist𝐞 recompensa de :', 'Casi te atrapa la policía pero lograste robar una cantidad valiosa de 💰. !Te cuidado la próxima vez! obtuviste:', 'Los mafiosos te han pagado :', 'Le has robado al Administrador del Grupo', 'Le robarte a tu presidente una sumar de :', 'le robarte a un famoso un valor de :'];
+global.robar = ['Violaste a la persona que dijo que los bots sea crean por termux y obtuviste :', 'Negociarte con el jefe de la mafia y obtuvist𝐞 recompensa de :', 'Casi te atrapa la policía pero lograste robar una cantidad valiosa de 💰. !Te cuidado la próxima vez! obtuviste:', 'Los mafiosos te han pagado :', 'Le has robado al Administrador del Grupo', 'Le robarte a tu presidente una sumar de :', 'le robarte a un famoso un valor de :'];
 global.robmal = ['LA POLICIA TE VIO 🙀👮‍♂️ PERDISTE', 'Fuiste a robar un banco 🏦 y tu ayudarte que vendio a la policía, perdiste', 'No pudiste escapar de la Policía 🚔🤡, perdiste :']
