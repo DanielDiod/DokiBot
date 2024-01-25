e9449b36bb3f8b407b48ad4e408d18195a6a7503
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-01-25 16:19:28 -0500

    Update noetiquetar.js

--- plugins/noetiquetar.js
+++ plugins/noetiquetar.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix, command, paypal }) => {
-conn.sendMessage(m.chat, {text:'*[ ⚠️ ] no etiquetes a unos de mis creadores si tienes alguna consulta hablales a su privado*'},  { quoted: m })
+conn.sendMessage(m.chat, {text:'*[ ⚠️ ] no etiquetes a unos de mis creadores si tienes alguna consulta o duda hablale a su privado solo por tema de Shana-Bot*'},  { quoted: m })
 }
 handler.help = ['pagina']
 handler.tags = ['info']
-handler.command = /^@51907913096$/i
+handler.command = /^@51907913096|@51977835959$/i
 export default handler
