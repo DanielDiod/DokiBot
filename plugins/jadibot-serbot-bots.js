bce6187eb0a0748a7e5812beaf691ad3ec31a84e
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-01-25 14:30:17 -0500

    Update jadibot-serbot-bots.js

--- plugins/jadibot-serbot-bots.js
+++ plugins/jadibot-serbot-bots.js
@@ -1,34 +1,25 @@
-import ws from 'ws';
-async function handler(m, { conn: _envio, usedPrefix }) {
-const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
-function convertirMsADiasHorasMinutosSegundos(ms) {
-var segundos = Math.floor(ms / 1000);
-var minutos = Math.floor(segundos / 60);
-var horas = Math.floor(minutos / 60);
-var días = Math.floor(horas / 24);
-segundos %= 60;
-minutos %= 60;
-horas %= 24;
-var resultado = "";
-if (días !== 0) {
-resultado += días + " días, ";
+async function handler(m, { conn, usedPrefix }) {
+  const connectedUsers = new Set()
+  const addedNumbers = new Set()
+  global.conns
+    .filter(conn => conn.user && conn.state !== 'close')
+    .forEach(user => {
+      const userJid = user.user.jid.replace(/[^0-9]/g, '');
+      if (!addedNumbers.has(userJid)) {
+        addedNumbers.add(userJid);
+        const userName = user.user.name || "Shana - Bot"
+        connectedUsers.add(`Wa.me/${userJid}?text=${usedPrefix}menu (${userName})`)
+      }
+    })
+  const connectedUserCount = connectedUsers.size
+  if (connectedUserCount > 0) {
+    const userList = [...connectedUsers].join(`\n`)
+    await m.reply(userList)
+  } else {
+    await m.reply('')
+  }
 }
-if (horas !== 0) {
-resultado += horas + " horas, ";
-}
-if (minutos !== 0) {
-resultado += minutos + " minutos, ";
-}
-if (segundos !== 0) {
-resultado += segundos + " segundos";
-}
-return resultado;
-}
-const message = users.map((v, index) => `👉🏻 wa.me/${v.user.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}estado (${v.user.name || '-'})\n*🔰 Tiempo activo :* ${ v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : "Desconocido"}`).join('\n\n')
-  const replyMessage = message.length === 0 ? '*No hay Sub bots conectado, verifique mas tardes.*' : message;
-const totalUsers = users.length;
-const responseMessage = `*[ 🛑 Aquí tienes la lista de algunos sub bots 🛑 ]*\n\n*👉🏻 Puedes contactarlos para ver si se unen a tu grupo*\n\n*Te pedimos de favor que:*\n*1.- Seas amable ✅*\n*2.- No insistas ni discutas ✅*\n\n*✳ ️Si le aparece el siguiente texto en blanco es que no hay ningún sub bot disponible en este momento inténtelo mas tarde*\n\n*_⚠ NOTA: ️ELLOS SON PERSONAS QUE NO CONOCEMOS.. POR LO QUE EL EQUIPO DE Monkibot NO SE HACE RESPONSABLE DE LO QUE PUEDA OCURRIR AHI.._*\n\n*SUBBOTS CONECTADO :* ${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim();
-await _envio.sendMessage(m.chat, {text: responseMessage, mentions: _envio.parseMention(responseMessage)}, {quoted: m})}
-handler.command = handler.help = ['listjadibot', 'bots', 'subsbots'];
-handler.tags = ['jadibot'];
-export default handler;
+handler.command = ['listjadibot', 'bots']
+handler.help = ['bots']
+handler.tags = ['serbot']
+export default handler
