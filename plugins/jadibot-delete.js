34f8e6c66359249973f4a8c275718c54f642cd25
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-01-25 14:34:05 -0500

    Update jadibot-delete.js

--- plugins/jadibot-delete.js
+++ plugins/jadibot-delete.js
@@ -1,14 +1,3 @@
-/*
-
-⚠ PROHIBIDO EDITAR ⚠ -- ⚠ PROHIBIDO EDITAR ⚠ -- ⚠ PROHIBIDO EDITAR ⚠
-
-El codigo de este archivo fue realizado por:
-- ReyEndymion (https://github.com/ReyEndymion)
-
-⚠ PROHIBIDO EDITAR ⚠ -- ⚠ PROHIBIDO EDITAR ⚠ -- ⚠ PROHIBIDO EDITAR ⚠
-
-*/
-
 import { readdirSync, statSync, unlinkSync, existsSync, readFileSync, watch, rmSync, promises as fs} from "fs"
 import path, { join } from 'path'
 let handler  = async (m, { conn }, args) => {
@@ -17,7 +6,7 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? c
 let uniqid = `${who.split`@`[0]}`
 if (global.conn.user.jid !== conn.user.jid) return conn.sendMessage(m.chat, {text: '*Use este comando directamente en el numero del Bot principal*'}, { quoted: m }) 
 else {
-await conn.sendMessage(m.chat, {text: "*👋 Adiós Bot, haz dejado de ser un Bot*"}, { quoted: m }) 
+await conn.sendMessage(m.chat, {text: "*👋🏻 Adiós Bot, haz dejado de ser un Bot*"}, { quoted: m }) 
 }
 try {
 fs.rmdir("./jadibts/" + uniqid, { recursive: true, force: true })
