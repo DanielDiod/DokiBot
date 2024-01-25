e459e6a74191707b6e5007d67047b08e1d1e4d0f
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-01-25 00:55:25 -0500

    Update descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 import cheerio from 'cheerio'
+import fg from 'api-dylux'
 
 let handler = async (m, { conn, args, command }) => {
-	if (!args[0]) return conn.reply(m.chat, '*🚩 Escribe la URL de un video de Facebook que deseas descargar.*', m)
+	if (!args[0]) return conn.reply(m.chat, '*• Ingresa el enlace de Facebook*', m)
 	try {
 	await m.react('🕓')
 	let config = {
@@ -20,15 +21,19 @@ let handler = async (m, { conn, args, command }) => {
       let $ = cheerio.load(data)
       let HD = $('div.container > div.results-download > ul > li:nth-child(1) > a').attr('href')
       let SD = $('div.container > div.results-download > ul > li:nth-child(2) > a').attr('href')
-	await conn.sendMessage(m.chat, { video: { url: HD || SD }, caption: null }, { quoted: estilo})
+	await conn.sendMessage(m.chat, { video: { url: HD || SD }, caption: `${global.listo}` }, { quoted: m})
 	await m.react('✅')
-	} catch (e) {
-		conn.reply(m.chat, '*☓ Ocurrió un error inesperado*', m).then(_ => m.react('✖️'))
-	}
-}
-handler.help = ['facebook'].map(v => v + ' <url fb>')
-handler.tags = ['downloader']
-handler.command = /^((facebook|fb)(downloder|dl)?)$/i
-handler.star = 2
-handler.register = true 
+	} catch {
+	try {
+    let result = await fg.fbdl(args[0])
+    await conn.sendFile(m.chat, result.videoUrl, 'fb.mp4', listo, m)
+    await m.react('✅')
+} catch {
+await conn.reply(m.chat, `${global.error}`, m).then(_ => m.react('✖️'))
+}}}
+handler.help = ['fb *<link fb>*']
+handler.tags = ['downloader'] 
+handler.command = /^(facebook|fb|facebookdl|fbdl)$/i
+handler.limit = 1
+handler.register = true
 export default handler
