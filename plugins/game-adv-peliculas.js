f0b7d2701e87e6c2b1943fe5c455ba3b8c2bf362
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-01-25 22:45:19 -0500

    Update game-adv-peliculas.js

--- plugins/game-adv-peliculas.js
+++ plugins/game-adv-peliculas.js
@@ -1,33 +1,63 @@
-import fs from 'fs'
-let timeout = 30000
-let poin = 600
+const peliculas = new Map()
+const movies = [
+    { emoji: "🚢🧊", title: "Titanic" },
+    { emoji: "🦖🌴", title: "Parque Jurásico" },
+    { emoji: "🦸‍♂️🦸‍♀️🤖", title: "Los Vengadores" },
+    { emoji: "🍫🏠📦", title: "Mi Pobre Angelito" },
+    { emoji: "🕷️🕸️🦸‍♂️", title: "El Hombre Araña" },
+    { emoji: "🏴‍☠️⚓🍻", title: "Piratas del Caribe" },
+    { emoji: "🔫🕵️‍♂️🕵️‍♀️", title: "Hombres de Negro" },
+    { emoji: "🧙‍♂️🔮🐉", title: "Harry Potter" },
+    { emoji: "🌍🌊🐟", title: "Buscando a Nemo" },
+    { emoji: "👑🦁🌅", title: "El Rey León" },
+    { emoji: "🚀🏌️‍♂️🌕", title: "Apollo 13" },
+    { emoji: "🧊❄️🏔️", title: "Frozen" },
+    { emoji: "🦖🦕🌋", title: "Parque Jurásico" },
+    { emoji: "🌿🐢🦖", title: "Parque Jurásico" },
+    { emoji: "👑🏰🧙‍♂️", title: "El Señor de los Anillos" },
+    { emoji: "🌚🔫🍹", title: "Pulp Fiction" },
+    { emoji: "🧠🔬🍃", title: "Milagros Inesperados" },
+    { emoji: "🚗🌪️👠", title: "Mad Max: Furia en el Camino" },
+    { emoji: "📖🥀🦁", title: "Las Crónicas de Narnia" },
+    { emoji: "🚁🏝️🐅", title: "Avatar" }
+]
 
-let handler = async (m, { conn, usedPrefix }) => {
-conn.tekateki = conn.tekateki ? conn.tekateki : {}
-let id = m.chat
-if (id in conn.tekateki) {
-conn.reply(m.chat, 'Todavía hay acertijos sin responder en este chat', conn.tekateki[id][0])
-throw false
+for (const movie of movies) {
+peliculas.set(movie.title.toLowerCase(), movie.emoji)
 }
-let tekateki = JSON.parse(fs.readFileSync(`./src/game/peliculas.json`))
-let json = tekateki[Math.floor(Math.random() * tekateki.length)]
-let _clue = json.response
-let clue = _clue.replace(/[A-Za-z]/g, '_')
-let caption = `
-ⷮ *${json.question}*
 
-*• Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
-*• Bono:* +${poin} Exp
-`.trim()
-conn.tekateki[id] = [
-await //conn.reply(m.chat, caption, m),
-conn.sendMessage(m.chat, { text: caption, contextInfo:{forwardingScore: 9999999, isForwarded: true, "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "body": `• ADIVINAN LA PELÍCULA CON EMOJIS •`, "previewType": "PHOTO", thumbnail: imagen1, sourceUrl: md}}}, { quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100}), json, poin, setTimeout(async () => {
-if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!\n*Respuesta:* ${json.response}`, conn.tekateki[id][0])
-delete conn.tekateki[id]
-}, timeout)
-]}
-handler.help = ['peliculas']
+const gam = new Map()
+function getRandomInt(min, max) {
+min = Math.ceil(min)
+max = Math.floor(max)
+return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+let handler = async (m, { conn }) => {
+let users = global.db.data.users[m.sender]
+let peli = movies[Math.floor(Math.random() * movies.length)]
+gam.set(m.sender, peli.title.toLowerCase())
+conn.reply(m.chat, `*• Adivina el nombre de la película con solo emojis:*\n\n${peli.emoji}`, m)
+}
+
+handler.before = async (m, { conn }) => {
+let users = global.db.data.users[m.sender]
+let pelicula = gam.get(m.sender)
+if (!pelicula) return
+if (m.text.toLowerCase() === pelicula) {
+const premio = ['2000', '3000', '1000']
+let prems = [7, 10, 15, 20, 5]
+let fee = prems[Math.floor(Math.random() * prems.length)]
+let ramdon = premio[Math.floor(Math.random() * premio.length)]
+users.exp += parseInt(ramdon)
+users.limit += fee
+conn.reply(m.chat, `*• Has ganado:* \n- ${ramdon} XP \n- ${fee} Coins💰`, m)
+gam.delete(m.sender);
+} else if (m.text.toLowerCase() === 'stop') {
+conn.reply(m.chat, `*• La película correcta era:* ${movies.find(movie => movie.title.toLowerCase() === pelicula).title}`, m)
+gam.delete(m.sender)
+}}
+handler.help = ['jueguito']
 handler.tags = ['game']
-handler.command = /^(advpe|adv|peliculas|pelicula)$/i
-handler.register = true
+handler.command = ['advpeli']
 export default handler
