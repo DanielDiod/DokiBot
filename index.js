96faedecdcbef750ddfe5e691745495078474ccd
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-01-27 17:40:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
  
-say('The Shana\nBot\nMD', {
+say('ShanaBot', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Por sopixdaniel`, {
+say(`by: Daniel`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']});
