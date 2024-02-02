22021d86349b4f1dc3776e9ecc281adf17e37430
Dali-13 <157506744+Dali-13@users.noreply.github.com>
2024-02-01 23:33:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -12,17 +12,17 @@ import { en, es, id, ar, pt } from './lib/idiomas/total-idiomas.js'
 
 global.owner = [ 
 ['51907913096', 'Daniel', true],
-['51977835959', 'sopiix', true],
-['51977835959'],
+['51967647592', 'Dalii', true],
+['51967647592'],
 ['51907913096'],
 ['51907913096'], 
 ['51906662557'],
 ['573183650526'],
 ['5492266466080'],
-['51977835959']]
+['51967647592']]
 
-global.mods = ['51977835959']
-global.prems = ['51977835959'],
+global.mods = ['51967647592']
+global.prems = ['51967647592'],
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
