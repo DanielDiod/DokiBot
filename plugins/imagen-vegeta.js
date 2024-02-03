b940db07eadef5af5cfaf7ea92509242fc905bb8
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-02-02 19:35:28 -0500

    Create imagen-vegeta.js

---
+++ plugins/imagen-vegeta.js
@@ -0,0 +1,23 @@
+let handler = async (m, {conn, command}) => {
+  let url = pack[Math.floor(Math.random() * pack.length)];
+    await conn.sendFile( 
+     m.chat, 
+     url, 
+     "gimage.jpg", 
+     ` 
+ Vegeta`.trim(), m)
+};
+handler.help = ["Vegeta"];
+handler.tags = ["internet"];
+handler.command = /^(Vegeta)$/i;
+export default handler;
+
+global.pack = [
+  "https://telegra.ph/file/5a382d2ee0f08306e9970.jpg",
+  "https://telegra.ph/file/ccdcd6a92c4f6b1c11a00.jpg",
+  "https://telegra.ph/file/260660a398e0d07360ba6.jpg",
+  "https://telegra.ph/file/aba8cbbe1a1c7dc727a19.jpg",
+  "https://telegra.ph/file/1fbb857f22b30578ae1b6.jpg",
+];
+
+handler.limit = 3;
