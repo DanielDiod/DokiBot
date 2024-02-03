85943db86c188040c0811f3cdc6a45048d390825
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-02-03 02:09:47 -0500

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,7 +1,7 @@
 let media = './Menu2.jpg'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let str = `*Hola usuario estos son los grupos ofc de Shana-bo*
+let str = `*Hola usuario estos son los grupos ofc de Shana-bot*
 
 📎 *Shana-Bot 🈴*
  
