3a766158665be36c7edf6457072d606898b5442c
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-02-03 03:08:10 -0500

    Update info-infobot.js

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -2,22 +2,26 @@ import db from '../lib/database.js'
 import { cpus as _cpus, totalmem, freemem, platform, hostname, version, release, arch } from 'os'
 import speed from 'performance-now'
 import { performance } from 'perf_hooks'
+import { sizeFormatter } from 'human-readable'
 
 let format = sizeFormatter({
     std: 'JEDEC',
-    decimalPlaces: 0,
+    decimalPlaces: 2,
     keepTrailingZeroes: false,
+    render: (literal, symbol) => `${literal} ${symbol}B`,
 })
 
 let handler = async (m, { conn, usedPrefix }) => {
    let bot = global.db.data.settings[conn.user.jid]
-   let uptime = process.uptime()
+   let _uptime = process.uptime() * 1000
+   let uptime = (_uptime).toTimeString()
    let totalreg = Object.keys(global.db.data.users).length
    const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
    const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
    const used = process.memoryUsage()
    const cpus = _cpus().map(cpu => {
       cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
+      return cpu
    })
    const cpu = cpus.reduce((last, cpu, _, { length }) => {
       last.total += cpu.total
