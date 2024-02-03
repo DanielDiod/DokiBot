2a3989ab511fa8c606801a5f4eca145b014fbf2c
Dali-13 <157506744+Dali-13@users.noreply.github.com>
2024-02-02 19:44:10 -0500

    Update imagen-cristiano.js

--- plugins/imagen-cristiano.js
+++ plugins/imagen-cristiano.js
@@ -13,11 +13,11 @@ handler.command = /^(cristiano)$/i;
 export default handler;
 
 global.pack = [
-  "https://telegra.ph/file/b7da157436bf29c5e8d21.jpg",
-  "https://telegra.ph/file/830088a43cc5f132a1657.jpg",
-  "https://telegra.ph/file/ca2311503805a502b3ccb.jpg",
-  "https://telegra.ph/file/3e01b5da8300f4d6bea0f.jpg",
-  "https://telegra.ph/file/a75a5f41e864b86210f42.jpg",
+  "https://telegra.ph/file/b20127fd54a1fd462faf2.jpg",
+  "https://telegra.ph/file/82b96fef625a47a4b1250.jpg",
+  "https://telegra.ph/file/9b2d2a129def992084406.jpg",
+  "https://telegra.ph/file/edb670744120a67b254d6.jpg",
+  "https://telegra.ph/file/2609b9d8006f4b96e223e.jpg",
 ];
 
 handler.limit = 3;
