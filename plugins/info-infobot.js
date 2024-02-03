da7c2a0f756fcae31b313a80eac7bffa0f6dbce7
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-02-03 02:22:20 -0500

    Update info-infobot.js

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -13,7 +13,6 @@ let format = sizeFormatter({
 let handler = async (m, { conn, usedPrefix }) => {
    let bot = global.db.data.settings[conn.user.jid]
    let _uptime = process.uptime() * 1000
-   let uptime = (_uptime).toTimeString()
    let totalreg = Object.keys(global.db.data.users).length
    const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
    const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
