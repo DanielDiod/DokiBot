d653a2e912c30d16bab84f3663ccd5596b10586b
Dali-13 <157506744+Dali-13@users.noreply.github.com>
2024-02-03 01:48:13 -0500

    Create imagen-spiderman.js

---
+++ plugins/imagen-spiderman.js
@@ -0,0 +1,23 @@
+let handler = async (m, {conn, command}) => {
+  let url = pack[Math.floor(Math.random() * pack.length)];
+    await conn.sendFile( 
+     m.chat, 
+     url, 
+     "gimage.jpg", 
+     ` 
+ Spiderman`.trim(), m)
+};
+handler.help = ["goku"];
+handler.tags = ["internet"];
+handler.command = /^(goku)$/i;
+export default handler;
+
+global.pack = [
+  "https://telegra.ph/file/f749593b7f9874317e546.jpg",
+  "https://telegra.ph/file/67625e3a7495777b4beff.jpg",
+  "https://telegra.ph/file/586c6c21829d57f71fab3.jpg",
+  "https://telegra.ph/file/90111b3b09a7ec599b13f.jpg",
+  "https://telegra.ph/file/7a4919ebd5687312b1e64.jpg",
+];
+
+handler.limit = 3;
