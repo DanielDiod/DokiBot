1b38373eb19610f40e183db0cc031e6e00088055
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-02-07 03:06:17 -0500

    Update buscador-applemusic.js

--- plugins/buscador-applemusic.js
+++ plugins/buscador-applemusic.js
@@ -1,10 +1,10 @@
-import yts from 'yt-search'
+import yts from 'music.apple-search'
 
 let handler = async (m, {conn, usedPrefix, text }) => {
    if (!text) return conn.reply(m.chat, '*🚩 Ingresa lo que deseas buscar en YouTube.*', m)
    await m.react('🕓')
    let results = await yts(text)
-   let res = results.all.map(v => v).filter(v => v.type == "video")
+   let res = results.all.map(v => v).filter(v => v.type == "audio")
    if (!res.length) return conn.reply(m.chat, 'No se encontraron resultados, intente con un nombre más Corto.', m).then(_ => m.react('✖️'))
    let txt = `*Apple - Search*`
    for (let i = 0; i < (30 <= res.length ? 30 : res.length); i++) {
@@ -13,7 +13,7 @@ let handler = async (m, {conn, usedPrefix, text }) => {
 	  txt += `	◦  *Duración* : ${res[i].timestamp || '×'}\n`
 	  txt += `	◦  *Publicado* : ${res[i].ago}\n`
 	  txt += `	◦  *Autor* : ${res[i].author.name || '×'}\n`
-	  txt += `	◦  *Url* : ${'https://music.apple.com/us/' + res[i].videoId}\n`
+	  txt += `	◦  *Url* : ${'https://music.apple.com/us/' + res[i].audioId}\n`
 	  }
    await conn.sendFile(m.chat, res[0].image, '', txt, m)
    await m.react('✅')
