15082870b8702b215c9c80505e5eab245c8b53d7
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-02-07 02:29:02 -0500

    Update buscador-applemusic.js

--- plugins/buscador-applemusic.js
+++ plugins/buscador-applemusic.js
@@ -9,7 +9,7 @@ if (!args[0]) throw `*Formato incorrecto*\nEjemplo:\n\n${usedPrefix + command} c
 try {
 let searchResults = await searchPornhub(args[0]);
 let teks = searchResults.result.map((v, i) => 
-`「 *P O R N H U B  - S E A R C H* 」
+`「 *APPLE  - S E A R C H* 」
 • *Título:* ${v.title}
 • *Duración:* ${v.duration}
 • *Vistas:* ${v.views}
@@ -21,7 +21,7 @@ teks = '*Sin resultados*';
 m.reply(teks);
 } catch (e) {
 }};
-handler.command = /^(phsearch|pornhubsearch)$/i;
+handler.command = /^(appleearch|applemusicsearch)$/i;
 export default handler;
 async function searchPornhub(search) {
   try {
@@ -33,7 +33,7 @@ async function searchPornhub(search) {
       const _title = $(b).find('a').attr('title');
       const _duration = $(b).find('var.duration').text().trim();
       const _views = $(b).find('var.views').text().trim();
-      const _url = 'https://music.apple.com' + $(b).find('a').attr('href');
+      const _url = 'https://music.apple.com/us/browse' + $(b).find('a').attr('href');
       const hasil = { title: _title, duration: _duration, views: _views, url: _url };
       result.push(hasil);
     });
