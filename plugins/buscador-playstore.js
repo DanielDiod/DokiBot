e987d01ca9016d89a742780d3f92fa9fc8964079
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-02-09 03:03:52 -0500

    Update buscador-playstore.js

--- plugins/buscador-playstore.js
+++ plugins/buscador-playstore.js
@@ -1,39 +1,29 @@
-import fetch from 'node-fetch'
-
-let handler = async (m, { conn, text, args }) => {
-	if (!args[0]) throw `*[❗] 𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙚𝙡 𝙣𝙤𝙢𝙗𝙧𝙚 𝙙𝙚 𝙡𝙖 𝘼𝙋𝙆 𝙦𝙪𝙚 𝙦𝙪𝙞𝙚𝙧𝙖 𝙗𝙪𝙨𝙘𝙖𝙧*`
-	let enc = encodeURIComponent(text)
-try {
-let json = await fetch(`https://latam-api.vercel.app/api/playstore?apikey=brunosobrino&q=${enc}`)
-let gPlay = await json.json()
-if (!gPlay.titulo) return m.reply(`[ ! ] Sin resultados`)
-conn.sendMessage(m.chat,{image:{url: gPlay.imagen},caption:`🔍 𝙍𝙚𝙨𝙪𝙡𝙩𝙖𝙙𝙤𝙨: ${gPlay.titulo}
-───────•••───────
-🧬 𝙄𝙙𝙚𝙣𝙩𝙞𝙛𝙞𝙘𝙖𝙙𝙤𝙧: ${gPlay.id}
-───────•••───────
-⛓️ 𝙇𝙞𝙣𝙠: ${gPlay.link}
-───────•••───────
-🖼️ 𝙄𝙢𝙖𝙜𝙚𝙣: ${gPlay.imagen}
-───────•••───────
-✍️ 𝘿𝙚𝙨𝙖𝙧𝙧𝙤𝙡𝙡𝙖𝙙𝙤𝙧: ${gPlay.desarrollador}
-───────•••───────
-📜 𝘿𝙚𝙨𝙘𝙧𝙞𝙥𝙘𝙞𝙤́𝙣: ${gPlay.descripcion}
-───────•••───────
-💲 𝙈𝙤𝙣𝙚𝙙𝙖: ${gPlay.moneda}
-───────•••───────
-🎭 𝙂𝙧𝙖𝙩𝙞𝙨?: ${gPlay.gratis}
-───────•••───────
-💸 𝙋𝙧𝙚𝙘𝙞𝙤: ${gPlay.precio}
-───────•••───────
-📈 𝙋𝙪𝙣𝙩𝙪𝙖𝙘𝙞𝙤́𝙣: ${gPlay.puntuacion}`},{quoted:m})
-} catch (e) {
-m.reply('𝙐𝙛 𝙚𝙧𝙧𝙤𝙧, 𝙨𝙚 𝙢𝙚 𝙘𝙖𝙮𝙤́ 𝙚𝙡 𝙨𝙚𝙧𝙫𝙞𝙙𝙤 🤡,  𝙫𝙪𝙚𝙡𝙫𝙖 𝙖 𝙞𝙣𝙩𝙚𝙣𝙩𝙖𝙧')
-console.log(e)
-}
-}
-
-handler.help = ['playstore <aplicacion>']
-handler.tags = ['internet']
-handler.command = /^(playstore)$/i
-
-export default handler 
+import fetch from 'node-fetch'; 
+ import translate from '@vitalets/google-translate-api'; 
+ const handler = async (m, {conn, text, args}) => { 
+   if (!args[0]) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙰𝙿𝙺 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*`; 
+   try { 
+     const enc = encodeURIComponent(text); 
+     const json = await fetch(`https://latam-api.vercel.app/api/playstore?apikey=Diego-Ofc&q=${enc}`); 
+     const gPlay = await json.json(); 
+  
+     const shadowgod = await translate(`${gPlay.descripcion}`, {to: 'es', autoCorrect: true}); 
+     if (!gPlay.titulo) return m.reply(`[ ! ] Sin resultados`); 
+     conn.sendMessage(m.chat, {image: {url: gPlay.imagen}, caption: `🔍 Resultado: ${gPlay.titulo} 
+ 🧬 Identificador: ${gPlay.id} 
+ ⛓️ Link: ${gPlay.link} 
+ 🖼️ Imagen: ${gPlay.imagen} 
+ ✍️ Desarrollador: ${gPlay.desarrollador} 
+ 📜 Descripcion: ${shadowgod.text} 
+ 💲 Moneda: ${gPlay.moneda} 
+ 🎭 Gratis?: ${gPlay.gratis} 
+ 💸 Precio: ${gPlay.precio} 
+ 📈 Puntuacion: ${gPlay.puntuacion}`}, {quoted: m}); 
+   } catch { 
+     await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*'); 
+   } 
+ }; 
+ handler.help = ['playstore <aplicacion>']; 
+ handler.tags = ['internet']; 
+ handler.command = /^(playstore)$/i; 
+ export default handler;
