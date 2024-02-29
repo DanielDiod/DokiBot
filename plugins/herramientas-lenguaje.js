bb233237daab26653395cdedbe23ba05ca49b71b
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-02-29 00:48:45 -0500

    Update herramientas-lenguaje.js

--- plugins/herramientas-lenguaje.js
+++ plugins/herramientas-lenguaje.js
@@ -1,27 +1,21 @@
 
-let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-	
-  let te = `✳️- es (Español)\n- en (Inglés)\n- id (Indonesia)\n- pt (Portugues)\n- ar (Árabe)\n\n📌 ${mssg.example}: *${usedPrefix + command}* en\nAyudanos a traducir el bot a tu idioma\nt.me/fgsupp_bot`
-  if (!text) throw te
-  let user = global.db.data.users[m.sender]
-   if (args[0] === "es") {
-       user.language = args[0]
-       m.reply("✅ *Español Seleccionado*\n\nAhora el bot responderá a su mensaje en Español")
-      } else if (args[0] === "en") {
-       user.language = args[0]
-       m.reply("✅ *Selected English*\n\nNow the bot will reply to your message in English")
-      } else if (args[0] === "id") {
-      user.language = args[0]
-       m.reply("✅ *Bahasa Indonesia terpilih*\n\nSekarang bot akan membalas pesanmu dengan bahasa Indonesia")
-      } else if (args[0] === "pt") {
-        user.language = args[0]
-         m.reply("✅ *Português selecionados*\n\nAgora o bot vai responder a sua mensagem em Português")
-       } else if (args[0] === "ar") {
-        user.language = args[0]
-        m.reply("✅ *تم اختيار اللغة العربية*\n\nالآن سيقوم البوت بالرد على رسائلك باللغة العربية")
-      } else {
-       m.reply(te)
-     }
+const { en, es, ar, id, pt, rs} = require('./libs/idiomas/total-idiomas.js')
+let user = global.db.data.users[m.sender]
+if (user.Language == 'es') {
+global.lenguaje = es
+} else if (user.Language == 'en') {
+global.lenguaje = en
+} else if (user.Language == 'ar') {
+global.lenguaje = ar 
+} else if (user.Language == 'id') { 
+global.lenguaje = id
+} else if (user.Language == 'pt') { 
+global.lenguaje = pt
+} else if (user.Language == 'rs') { 
+global.lenguaje = rs
+} else {
+global.lenguaje = es
+}
     
  }
  handler.help = ['language <es-en..>']
