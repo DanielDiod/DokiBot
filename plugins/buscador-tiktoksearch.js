a61bad525d90a2e8a5b0b1db188115c51b1dc9e1
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-03-09 23:59:45 -0500

    Update buscador-tiktoksearch.js

--- plugins/buscador-tiktoksearch.js
+++ plugins/buscador-tiktoksearch.js
@@ -1,34 +1,47 @@
 //Este comando aun falta hacer algunos arreglos
 
-import fetch from 'node-fetch';
-
-const handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `*[⚠️ INFORMACIÓN ⚠️]*\n\nuso del comando correcto:\n\n${usedPrefix + command} edits de mia kalifa`;;
-  const res = await fetch(`https://api.ikyy.my.id/tiktoksearch?text=${text}`);	
-  const data = await res.json();
-  const json = data.result[0];
-  await conn.sendFile(m.chat, json.play, 'tiktok.mp4', `
-*T I K T O K  - S E A R C H *
-
-*Author👤:* ${json.author}\n
-*Region 🌍:* ${json.region}\n
-*Duración ⌛:* ${json.duration} detik\n
-*Titulo 📋:* ${json.title}
-
-${vs}`, m);
-  await conn.sendFile(m.chat, json.music, 'error.mp3', null, m, true);
-};
-
-handler.help = ['tiktoksearch'];
-handler.command = /^(tiktoksearch|ttsearch|tiktoks|ttsr)$/i;
+const fetch = require('node-fetch');
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+ if (!text) throw `🚩 *Example:* ${usedPrefix + command} anime`
+  try {
+    const res = await fetch(`https://api.botcahx.eu.org/api/search/tiktoks?query=${text}&apikey=${btc}`);
+    const api = await res.json(); 
+    const randomIndex = Math.floor(Math.random() * api.result.data.length);
+    let video = api.result.data[randomIndex];
+    let capt = `乂 *T I K T O K  S E A R C H*\n\n`;
+    capt += `  ◦ *Video ${randomIndex + 1}*\n`;
+    capt += `  ◦ *Video ID*: ${video.video_id}\n`;
+    capt += `  ◦ *Region*: ${video.region}\n`;
+    capt += `  ◦ *Title*: ${video.title}\n`;
+    capt += `  ◦ *Duration*: ${video.duration} seconds\n`;
+    capt += `  ◦ *Music Info:*\n`;
+    capt += `  ◦ *ID*: ${video.music_info.id}\n`;
+    capt += `  ◦ *Title*: ${video.music_info.title}\n`;
+    capt += `  ◦ *Author*: ${video.music_info.author}\n`;
+    capt += `  ◦ *Original*: ${video.music_info.original ? "Yes" : "No"}\n`;
+    capt += `  ◦ *Duration*: ${video.music_info.duration} seconds\n`;
+    capt += `  ◦ *Album*: ${video.music_info.album}\n`;
+    capt += `  ◦ *Play Count*: ${video.play_count}\n`;
+    capt += `  ◦ *Digg Count*: ${video.digg_count}\n`;
+    capt += `  ◦ *Comment Count*: ${video.comment_count}\n`;
+    capt += `  ◦ *Share Count*: ${video.share_count}\n`;
+    capt += `  ◦ *Download Count*: ${video.download_count}\n`;
+    capt += `  ◦ *Created Time*: ${new Date(video.create_time * 1000).toUTCString()}\n`;
+    capt += `  ◦ *Is Ad*: ${video.is_ad ? "Yes" : "No"}\n`;
+    capt += `  ◦ *Is Top*: ${video.is_top ? "Yes" : "No"}\n`;
+    capt += `  ◦ *Author:*\n`;
+    capt += `  ◦ *ID*: ${video.author.id}\n`;
+    capt += `  ◦ *Unique ID*: ${video.author.unique_id}\n`;
+    capt += `  ◦ *Nickname*: ${video.author.nickname}\n`;
+    capt += `\n`;
+    conn.sendFile(m.chat, video.play, null, capt, m);
+  } catch (error) {
+    throw `🚩 *Video Tidak Ditemukan!*`
+  }
+}
+handler.help = ['ttsearch'].map(v => v + ' <username>');
 handler.tags = ['downloader'];
+handler.command = /^(tiktoksearch|ttsearch)$/i;
 handler.limit = true;
-handler.group = false;
-handler.premium = false;
-handler.owner = false;
-handler.admin = false;
-handler.botAdmin = false;
-handler.fail = null;
-handler.private = false;
 
-export default handler;
+module.exports = handler;
