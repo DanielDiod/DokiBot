3930659279419fe9c32af6a206eb83a8c2bae225
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-04-06 00:13:36 -0500

    Update Prueba-Copilot.js

--- plugins/Prueba-Copilot.js
+++ plugins/Prueba-Copilot.js
@@ -1,15 +1,2 @@
-import fetch from 'node-fetch';
-import axios from 'axios';
-import translate from '@vitalets/google-translate-api';
-import {Configuration, OpenAIApi} from 'openai';
-try { 
-if(!q) return await enviar('𝙄𝙣𝙜𝙧𝙚𝙨𝙖 𝙐𝙣𝙖 𝙋𝙧𝙚𝙜𝙪𝙣𝙩𝙖 𝙀𝙟𝙥: `!geminis cuando inicio one pice`') 
-let openia = await fetchJson(`https://controlled-gae-deliriusapi.koyeb.app/api/gemini?query=${q}`)
-await asta.sendMessage(from, {image: {url: `https://telegra.ph/file/93d066df4bfce347056e8.jpg`}, caption: `${openia.message}`}, {quoted: info})
-} catch (e) {
-console.log(openia)
-  return enviar('𝙚𝙧𝙧𝙤𝙧')
-}}}};
-handler.command = /^(gemini)$/i;
-export default handler;
+no
 
