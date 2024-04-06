45216efe77f69adae3c5edcbd412dcec426858bf
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-04-05 23:51:39 -0500

    Update Prueba-Copilot.js

--- plugins/Prueba-Copilot.js
+++ plugins/Prueba-Copilot.js
@@ -1,1 +1,11 @@
-no
+case 'geminis': 
+case 'gemini':
+try { 
+if(!q) return await enviar('𝙄𝙣𝙜𝙧𝙚𝙨𝙖 𝙐𝙣𝙖 𝙋𝙧𝙚𝙜𝙪𝙣𝙩𝙖 𝙀𝙟𝙥: `!geminis cuando inicio one pice`') 
+let openia = await fetchJson(`https://controlled-gae-deliriusapi.koyeb.app/api/gemini?query=${q}`)
+await asta.sendMessage(from, {image: {url: `https://telegra.ph/file/93d066df4bfce347056e8.jpg`}, caption: `${openia.message}`}, {quoted: info})
+} catch (e) {
+console.log(openia)
+  return enviar('𝙚𝙧𝙧𝙤𝙧');
+}
+break
