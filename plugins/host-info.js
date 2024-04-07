795d239894877409099c1123817a77d1caf00e2d
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-04-07 12:56:23 -0500

    Actualizar host-info.js

--- plugins/host-info.js
+++ plugins/host-info.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let text = `💫 Te presentamos un nuevo hosting: Infinity-Wa Host`
 
-`*¿Muy lento tu nokia y necesitas tener activo tu bot 24/7?*
+*¿Muy lento tu nokia y necesitas tener activo tu bot 24/7?*
 
 > *Te tenemos la mejor opción para mantener activo tu bot 24/7, a precios muy accesibles. Es muy barato y todos pueden comprar.*
 
