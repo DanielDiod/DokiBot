124394448ce5c21a28ff692218a42fc548f1f727
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-04-07 12:41:03 -0500

    Actualizar host-info.js

--- plugins/host-info.js
+++ plugins/host-info.js
@@ -1,1 +1,26 @@
-.
+let media = './storage/logos/Menu2.jpg'
+let handler = async (m, { conn, command }) => {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+let str = `💫 Te presentamos un nuevo hosting: *Infinity-Wa Host*`
+
+`*¿Muy lento tu nokia y necesitas tener activo tu bot 24/7?*
+
+> *Te tenemos la mejor opción para mantener activo tu bot 24/7, a precios muy accesibles. Es muy barato y todos pueden comprar.*
+
+*🚩Precios:*
+* 1GB, 100 CPU = 1Dolar
+* 2GB, 120 CPU = 2Dolar
+* 3GB, 140 CPU = 3Dolar
+* 4GB, 175 CPU = 4Dolar
+* 5GB, 200 CPU = 5 Dólar. 
+
+*Contactanos para más información o alquidir los servicios:*
+https://chat.whatsapp.com/GQ82mPnSYnm0XL2hLPk7FV
+
+> *Tenemos varios métodos de pagos: PayPal, Mercado pago, Naranja x, Yape, Transferencia por banco.*`
+await conn.sendFile(m.chat, media, 'Curiosity.jpg', str, fkontak)}
+handler.tags = ['main']
+handler.command = /^info|host|pagos$/i
+handler.exp = 35
+handler.register = true
+export default handler
