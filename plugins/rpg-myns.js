95c95e9ead310b9a6724057193f5c2919741ce20
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-04-20 18:43:06 -0500

    Update rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -1,11 +1,15 @@
-import { createHash } from 'crypto'
-let handler = async function (m, { conn, text, usedPrefix }) {
-let sn = createHash('md5').update(m.sender).digest('hex')
-await m.reply(`⬇️ *ᴇsᴛᴇ ᴇs sᴜs ɴᴜᴍᴇʀᴏ ᴅᴇʟ sᴇʀɪᴇ * ⬇️`.trim())
-await m.reply(`${sn}`.trim())
-}
-handler.help = ['myns']
-handler.tags = ['xp']
-handler.command = /^(myns|ceksn)$/i
-handler.register = true
-export default handler
+import {createHash} from "crypto";
+let handler = async function (m, {conn, text, usedPrefix}) {
+  let sn = createHash("md5").update(m.sender).digest("hex");
+  m.reply(
+    `┏┅ ━━━━━━━━━━━━ ┅ ━
+┃ *Este es tu número de serie 👇🏻:* 
+┃ ${sn}
+┗┅ ━━━━━━━━━━━━ ┅ ━`.trim()
+  );
+};
+handler.help = ["myns"];
+handler.tags = ["xp"];
+handler.command = /^(myns|ceksn)$/i;
+handler.register = true;
+export default handler;
