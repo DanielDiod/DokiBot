6fc786d9aa850cfc89051da54910141b76bd2512
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-04-21 00:34:33 -0500

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -168,7 +168,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     let pp15 = 'https://tinyurl.com/ykv7g4zy'
     m.react('⭐')
    
-   await conn.reply(m.chat, '*Espere un momento..*', m, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: 'WhatsApp Bot Multi Device', body: [wm, '' + lb + '🪙', '🈴'].getRandom(), sourceUrl: global.ig, thumbnail: gataImg.getRandom()}}})
+   await conn.reply(m.chat, '*Espere un momento.. @${m.sender.split`@`[0]}* estamos enviando el menu', m, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: 'WhatsApp Bot Multi Device', body: [wm, '' + lb + '🪙', '🈴'].getRandom(), sourceUrl: global.ig, thumbnail: gataImg.getRandom()}}})
 
    
     conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: m })
