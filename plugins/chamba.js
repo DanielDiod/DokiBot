274c03a372bb729be049e01eccf158113cf11b21
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-04-20 21:50:32 -0500

    Update chamba.js

--- plugins/chamba.js
+++ plugins/chamba.js
@@ -7,20 +7,16 @@ _${usedPrefix}ytmp4_`.trim();
   }
 
   if (command == "chambearhub") {
-    var play = `*CHAMBEANDO..*\n\n*Estuviste como regalona en el oxxo, bailandole a 9 negros 🗿*..`.trim();
-    m.reply(play);
-}
-
-  if (command == "chambear1") {
-    var play = `*CHAMBEANDO..*\n\n*Estuviste chambeando y llegaron 3 negros a violarte*..`.trim();
-    m.reply(play);
-}
-
-  if (command == "chambear2") {
-    var play = `*CHAMBEANDO..*\n\n*Estuviste chambeando y 5 negros se corrieron en tu boca*..`.trim();
+    var play = `${pickRandom(global.piropo)}*..`.trim();
     m.reply(play);
   }
 };
 handler.command = ["chambear1", "chambear2", "chambearhub"];
 handler.tags = ["internet"];
 export default handler
+global.piropo = ["CHAMBEANDO..
+
+Estuviste chambeando y 5 negros se corrieron en tu boca..,
+CHAMBEANDO..
+
+Estuviste chambeando y llegaron 3 negros a violarte..,"
