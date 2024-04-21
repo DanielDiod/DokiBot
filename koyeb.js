84bda848323af9905dfa2d8e4bcd101dbe2cabcc
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-04-21 01:20:28 -0500

    Update koyeb.js

--- koyeb.js
+++ koyeb.js
@@ -5,11 +5,11 @@ RUN sudo dnf -y update &&\
     sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
     sudo dnf clean all -y
 
-RUN git clone https://github.com/elrebelde21/The-LoliBot-MD
+RUN git clone https://github.com/DanielDiod/DokiBot
 
-WORKDIR /root/The-LoliBot-MD
+WORKDIR /root/DokiBot
 
-COPY ./root/The-LoliBot-MD
+COPY ./root/DokiBot
 
 RUN yarn install
 
