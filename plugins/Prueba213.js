3f16c7d837c185eca5136a09db64d2cfe1d0f6b3
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-04-25 15:27:52 -0500

    Update Prueba213.js

--- plugins/Prueba213.js
+++ plugins/Prueba213.js
@@ -1,15 +1,1 @@
-import Scraper from "@SumiFX/Scraper"
-
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return m.reply('🍭 Ingresa el nombre de la imágen que estas buscando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Yumeku`)
-try {
-let { dl_url } = await Scraper.pinterest(text)
-await conn.sendFile(m.chat, 'thumbnail': gataImg.getRandom(), null, m)
-} catch {
-}}
-handler.help = ['pinterest <búsqueda>']
-handler.tags = ['img']
-handler.command = ['prueba']
-handler.register = true 
-//handler.limit = 1
-export default handler
+no
