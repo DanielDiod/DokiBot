dddd441de064252bc26bc4aa8d543e814c44b161
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-04-27 01:28:51 -0500

    Update Prueba213.js

--- plugins/Prueba213.js
+++ plugins/Prueba213.js
@@ -1,17 +1,1 @@
-import fetch from 'node-fetch'
-
-let handler = async (m, { conn, text }) => {
-  if (!text) throw 'Que quieres crear?'
-  let msg = encodeURIComponent(text)
-  let res = await fetch(`https://aemt.me/bingimg?text=${msg}`)
-  let data = await res.json()
-  console.log(data)
-  let buffer = data.result
-  conn.sendFile(m.chat, buffer, 'image.png', `${text}`, m)
-}
-
-handler.help = ['bingimg <query>']
-handler.tags = ['AI']
-handler.command = /^bingimg$/i
-
-export default handler
+no
