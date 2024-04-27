080018e89abfec45a5b149c5feeb9725cefea95b
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-04-27 01:27:10 -0500

    Update Prueba213.js

--- plugins/Prueba213.js
+++ plugins/Prueba213.js
@@ -2,14 +2,12 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text }) => {
   if (!text) throw 'Que quieres crear?'
-  m.react(⌛)
   let msg = encodeURIComponent(text)
   let res = await fetch(`https://aemt.me/bingimg?text=${msg}`)
   let data = await res.json()
   console.log(data)
   let buffer = data.result
   conn.sendFile(m.chat, buffer, 'image.png', `${text}`, m)
-  m.react(✅)
 }
 
 handler.help = ['bingimg <query>']
