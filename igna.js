f9b4d2eae8af967e2ccb6f2ccfb4b49b1b0b645e
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-04-29 00:18:02 -0500

    Update igna.js

--- igna.js
+++ igna.js
@@ -1,3 +1,1 @@
-case 'plist':
-conn.relayMessage(from, { viewOnceMessage: { message: { interactiveMessage: { header: { title: 'Lista' }, body: { text: '💤' }, nativeFlowMessage: { buttons: [ { name: 'single_select', buttonParamsJson: JSON.stringify({ title: 'Click', sections: [ {title: 'Lista', highlight_label: 'Yaoi', rows: [{ title: 'menu1', id: 'menu1' }]}, { highlight_label: 'ON', rows: [{ header: 'Test', title: 's',description: 's', id: 'tes'}] }, { highlight_label: 'ON', rows: [ { header: 'Test', title: 'status', description: 'status', id: 'te' }]}]})}], messageParamsJson: '' }}}}}, {})		
-break
+
