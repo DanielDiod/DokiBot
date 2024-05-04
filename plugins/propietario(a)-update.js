1c04da6762d396a2c2ff29aa56b4249bf95cb6ac
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-04 12:37:53 -0500

    Actualizar propietario(a)-update.js

--- plugins/propietario(a)-update.js
+++ plugins/propietario(a)-update.js
@@ -1,17 +1,41 @@
 import { execSync } from 'child_process'
-let handler = async (m, { conn, text }) => {
-try {  
-if (global.conn.user.jid == conn.user.jid) {
-let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-conn.reply(m.chat, messager, m, fake,)
-//} catch {
-var update = execSync('git remote set-url origin https://github.com/DanielDiod/DokiBot.git && git pull')
-await m.reply(update.toString())
-} catch {
-await m.reply(`${fg}`) 
-}}
-handler.help = ['update']
-handler.tags = ['owner'] 
-handler.command = /^update|actualizar$/i
+
+var handler = async (m, { conn, text }) => {
+
+try {
+const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
+let messager = stdout.toString()
+if (messager.includes('Already up to date.')) messager = 'Atención \n *No.hay Actualizaciones pendientes*'
+if (messager.includes('Updating')) messager = 'Atención\n *actualización finalizada correctamente*\n\n' + stdout.toString()
+conn.reply(m.chat, messager, m, fake, )
+} catch { 
+try {
+const status = execSync('git status --porcelain')
+if (status.length > 0) {
+const conflictedFiles = status.toString().split('\n').filter(line => line.trim() !== '').map(line => {
+if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('sessions/') || line.includes('npm-debug.log')) {
+return null
+}
+return '*→ ' + line.slice(3) + '*'}).filter(Boolean)
+if (conflictedFiles.length > 0) {
+const errorMessage = `🚩 *Se han hecho cambios locales en archivos del bot que entran en conflicto con las actualizaciones del repositorio. Para actualizar, reinstala el bot o realiza las actualizaciones manualmente*\n\nArchivos en conflicto:\n\n${conflictedFiles.join('\n')}`
+await conn.reply(m.chat, errorMessage, m, fake, )
+}
+}
+} catch (error) {
+console.error(error)
+let errorMessage2 = '🚩 *Ocurrió un fallo. Por favor, inténtalo de nuevo más tarde*'
+if (error.message) {
+errorMessage2 += '\n*- Mensaje de error:* ' + error.message;
+}
+await conn.reply(m.chat, errorMessage2, m, fake, )
+}
+}
+
+}
+handler.help = ['update', 'actualizar']
+handler.tags = ['own']
+handler.command = /^(update|actualizar|gitpull)$/i
 handler.rowner = true
-export default handler 
+
+export default handler
