f1ce52ac841bc09dc3a1ad58373afd4939b9fae4
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-05-05 18:56:48 -0500

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -10,7 +10,7 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const oi = `${pesan}`;
-  let teks = `*Shana-Bot*\n\n *Integrantes :  ${participants.length}* ${oi}\n\n┌──⭓ Ya revivan\n`;
+  let teks = `*Igna Bot*\n\n *Integrantes :  ${participants.length}* ${oi}\n\n┌──⭓ Ya revivan\n`;
   for (const mem of participants) {
     teks += `│⭔ @${mem.id.split('@')[0]}\n`;
   }
