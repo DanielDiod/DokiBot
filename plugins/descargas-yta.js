693bc7de03d9a9b1d0e307236a6cdd05e6f57aec
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-05-05 18:54:24 -0500

    Update descargas-yta.js

--- plugins/descargas-yta.js
+++ plugins/descargas-yta.js
@@ -5,7 +5,7 @@ import fetch from 'node-fetch'
 let limit = 100
 
 let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
-if (!args || !args[0]) conn.reply(m.chat, `*🚩 Escribe la URL de un video de YouTube que deseas descargar.*`, m)
+if (!args || !args[0]) conn.reply(m.chat, `*🚩 Escribe la URL de un video de YouTube que deseas descargar.*`,  m, fake,)
 if (!args[0].match(/youtu/gi)) return conn.reply(m.chat, `Verifica que la *URL* sea de YouTube`, m).then(_ => m.react('✖️'))
 let q = '128kbps'
 
@@ -42,7 +42,7 @@ let { title, size, dl_url } = yt
 let vid = (await yts(text)).all[0]
 let { thumbnail, url } = vid
 
-if (size.split('MB')[0] >= limit) return conn.reply(m.chat,`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m).then(_ => m.react('✖️'))
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat,`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`,  m, fake,).then(_ => m.react('✖️'))
 
 await conn.sendMessage(m.chat, {
         text: `  🍭 *Título ∙* ${title}\n\n⚖️ *Tamaño ∙* ${size}\n\n*↻ Espera @${m.sender.split`@`[0]}, soy lenta. . .* .`,
@@ -62,7 +62,7 @@ sourceUrl: `${url}`,
 thumbnail: await (await fetch(vid.thumbnail)).buffer()}}}, { quoted: m })
 await m.react('✅')
 } catch {
-await conn.reply(m.chat, `*☓ Ocurrió un error inesperado*`, m).then(_ => m.react('✖️'))
+await conn.reply(m.chat, `*☓ Ocurrió un error inesperado*`,  m, fake,).then(_ => m.react('✖️'))
 console.error(error)
 }}}
 handler.help = ['ytmp3 <url yt>']
