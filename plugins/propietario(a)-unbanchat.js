c0b303d6c4633283cf689fc3c347b80b0ef3368f
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-05-05 19:45:59 -0500

    Update propietario(a)-unbanchat.js

--- plugins/propietario(a)-unbanchat.js
+++ plugins/propietario(a)-unbanchat.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
-  if (!(m.chat in global.db.data.chats)) return m.reply('*Este chat no está registrado en la base de datos!*')
+  if (!(m.chat in global.db.data.chats)) return m.reply('*Este chat no está registrado en la base de datos!*', m, fake,)
   let chat = global.db.data.chats[m.chat]
-  if (!chat.isBanned) return m.reply('*Este chat no está baneado!!*')
+  if (!chat.isBanned) return m.reply('*Este chat no está baneado!!*', m, fake,)
   chat.isBanned = false
-  m.reply(`*BOT ONLINE 🚀*`)
+  m.reply(`*BOT ONLINE 🚀*`, m, fake,)
 }
 handler.command = /^unbanchat$/i
 handler.botAdmin = true
