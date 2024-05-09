c1605d8c39f2c0a793d3ee8e415fb83da92eeca8
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-05-09 11:41:58 -0500

    Update descargas-playlist.js

--- plugins/descargas-playlist.js
+++ plugins/descargas-playlist.js
@@ -1,25 +1,95 @@
-import yts from 'yt-search'
+import yts from 'yt-search';
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+if (!text) conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙉 𝙑𝙄𝘿𝙀𝙊 𝙊 𝘾𝘼𝙉𝘼𝙇 𝘿𝙀 𝙔𝙊𝙐𝙏𝙐𝘽𝙀\n\n𝙒𝙍𝙄𝙏𝙀 𝙏𝙃𝙀 𝙉𝘼𝙈𝙀 𝙊𝙁 𝘼 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝙑𝙄𝘿𝙀𝙊 𝙊𝙍 𝘾𝙃𝘼𝙉𝙉𝙀𝙇`, fkontak,  m)
+try {
+    let result = await yts(text);
+    let ytres = result.videos;
+  let teskd = `𝘽𝙪𝙨𝙦𝙪𝙚𝙙𝙖 𝙙𝙚 *${text}*`
+    
+let listSections = [];
+for (let index in ytres) {
+        let v = ytres[index];
+        listSections.push({
+         title: `${htki} 𝙍𝙀𝙎𝙐𝙇𝙏𝘼𝘿𝙊𝙎 ${htka}`,
+            rows: [
+                {
+                    header: '𝗔 𝗨 𝗗 𝗜 𝗢',
+                    title: "",
+                    description: `${v.title} | ${v.timestamp}\n`, 
+                    id: `${usedPrefix}play.1 ${v.url}`
+                },
+                {
+                    header: "𝗩 𝗜 𝗗 𝗘 𝗢",
+                    title: "" ,
+                    description: `${v.title} | ${v.timestamp}\n`, 
+                    id: `${usedPrefix}play.2 ${v.url}`
+                }, 
+              {
+                    header: "𝗔 𝗨 𝗗 𝗜 𝗢   𝗗 𝗢 𝗖",
+                    title: "" ,
+                    description: `${v.title} | ${v.timestamp}\n`, 
+                    id: `${usedPrefix}ytmp3doc ${v.url}`
+                }, 
+                {
+                    header: "𝗩 𝗜 𝗗 𝗘 𝗢   𝗗 𝗢 𝗖",
+                    title: "" ,
+                    description: `${v.title} | ${v.timestamp}\n`, 
+                    id: `${usedPrefix}ytmp4doc ${v.url}`
+                }
+            ]
+        });
+    }
+await conn.sendList(m.chat, `${htki} *𝙍𝙀𝙎𝙐𝙇𝙏𝘼𝘿𝙊𝙎* ${htka}\n`, `\n𝘽𝙪𝙨𝙦𝙪𝙚𝙙𝙖 𝙙𝙚: ${text}`, `𝗕 𝗨 𝗦 𝗖 𝗔 𝗥`, listSections, m);
+} catch (e) {
+await conn.sendButton(m.chat, `\n${wm}`, lenguajeGB['smsMalError3']() + '#report ' + usedPrefix + command, null, [[lenguajeGB.smsMensError1(), `#reporte ${lenguajeGB['smsMensError2']()} *${usedPrefix + command}*`]], null, null, m)
+console.log(e) 
+}}
+handler.help = ['playlist']
+handler.tags = ['dl']
+handler.command = /^playlist|ytbuscar|yts(earch)?$/i
+handler.limit = 1
+handler.level = 3
 
-let handler = async (m, {conn, usedPrefix, text }) => {
-   if (!text) return conn.reply(m.chat, '*🚩 Ingresa lo que deseas buscar en YouTube.*', m, fake,)
-   await m.react('🕓')
-   let results = await yts(text)
-   let res = results.all.map(v => v).filter(v => v.type == "video")
-   if (!res.length) return conn.reply(m.chat, 'No se encontraron resultados, intente con un nombre más Corto.', m, fake,).then(_ => m.react('✖️'))
-   let txt = `*YouTube - Search*`
-   for (let i = 0; i < (30 <= res.length ? 30 : res.length); i++) {
-      txt += `\n\n`
-	  txt += `	◦  *Titulo* : ${res[i].title}\n`
-	  txt += `	◦  *Duración* : ${res[i].timestamp || '×'}\n`
-	  txt += `	◦  *Publicado* : ${res[i].ago}\n`
-	  txt += `	◦  *Autor* : ${res[i].author.name || '×'}\n`
-	  txt += `	◦  *Url* : ${'https://youtu.be/' + res[i].videoId}\n`
-	  }
-   await conn.sendFile(m.chat, res[0].image, '', txt, m, fake,)
-   await m.react('✅')
-}
-handler.help = ['ytsearch']
-handler.tags = ['search']
-handler.command = ['ytsearch', 'yts']
-handler.register = true 
 export default handler
+
+
+/*import yts from 'yt-search';
+import fs from 'fs';
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) throw `${lenguajeGB['smsAvisoMG']()}${mid.smsMalused4}`
+try {
+let vids_ = { 
+from: m.sender, 
+urls: [] 
+}
+if (!global.videoList) {
+global.videoList = [];
+}
+if (global.videoList[0]?.from == m.sender) {
+delete global.videoList;
+}
+let results = await yts(text);
+let textoInfo = `${lenguajeGB['smsAvisoIIG']()}${mid.smsYtlist(usedPrefix)}
+*${usedPrefix}video 2*\n\n••••••••••••••••••••••••••••••••••••`.trim()  
+let teks = results.all.map((v, i) => {
+let link = v.url;
+vids_.urls.push(link);
+return `[${i + 1}]\n❤️꙰༻ *${mid.smsYT1}:*  ${v.title}
+⁖🩵꙰༻ *${mid.smsYT4}:* ${v.url}
+⁖💜꙰༻ *${mid.smsYT5}:* ${v.timestamp}
+⁖💚꙰༻ *${mid.smsYT9}:* ${v.ago}
+⁖🧡꙰༻ *${mid.smsYT10}:* ${v.views}`}).join('\n\n••••••••••••••••••••••••••••••••••••\n\n')
+conn.sendFile(m.chat, results.all[0].thumbnail, 'yts.jpeg', textoInfo + '\n\n' + teks, fkontak, m)
+global.videoList.push(vids_);
+} catch (e)  {    
+console.log(e)
+handler.limit = 0
+}}
+handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
+handler.tags = ['tools']
+handler.command = /^playlist|ytbuscar|yts(earch)?$/i
+handler.register = true
+handler.limit = 1
+handler.level = 3
+export default handler
+*/
