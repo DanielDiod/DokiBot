b541f7dd58b96ce09962c90c6205befc118ce365
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-05-09 23:57:00 -0500

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -29,8 +29,8 @@ const texto1 = `╭──────────✰\n
         ╰──────────✰\n\n`.trim()
 		
 await conn.sendButton(m.chat, wm, texto1, res.videos[0].thumbnail, [
-	['mp4 doc', `${usedPrefix}ytmp3doc`],
-	['mp3 doc', `${usedPrefix}ytmp3doc`]
+	['mp4 doc', `${usedPrefix}ytmp3doc ${url}`],
+	['mp3 doc', `${usedPrefix}ytmp3doc ${url}`]
   ], null, [['Canal', `${md}`]], m)
        
        try {
