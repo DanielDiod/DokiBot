f1fe68e7ef09cd2c53f152aaa156a224dd50f28c
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-05-09 20:21:50 -0500

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -1,139 +1,30 @@
-import fg from 'api-dylux'
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
+
 import yts from 'yt-search'
-import fetch from 'node-fetch' 
-let [feature, inputs, inputs_, inputs__, inputs___] = text.split(" ")
-let handler = async (m, { conn, args, usedPrefix, text, command }) => {
-    ]
-    let [feature, inputs, inputs_, inputs__, inputs___] = text.split(" ")
-   if (!text) throw `y la musica ?`
-    if (lister.includes(feature)) {
-        if (feature == "mp3") {
-            if (!inputs) return conn.reply(m.chat, `*🚩 Ingresa el titulo de un video o musica de YouTube.*`,  m, fake,)
-    await m.react('🕓')
-    let res = await yts(text)
-    let vid = res.videos[0]
-    let q = '128kbps'
-const texto1 = `╭──────────✰\n
-	│🍭 *Título ∙* ${vid.title}\n
-        │🕜 *Duración ∙* ${vid.timestamp}\n
-        │👁 *Visitas ∙* ${vid.views}\n
-        │📚 *Autor ∙* ${vid.author.name}\n
-        │📅 *Publicado ∙* ${vid.ago}\n
-        │⛓ *Url ∙* ${'https://youtu.be/' + vid.videoId}\n
-        ╰──────────✰\n\n`.trim()
-		
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+	
+  if (!text) throw `Ingrese el titulo de la musica de youtube`
+	let res = await yts(text)
+	let vid = res.videos[0]
+	if (!vid) throw `✳️ Vídeo/Audio no encontrado`
+	let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid
+	//const url = 'https://www.youtube.com/watch?v=' + videoId
+	m.react('🎧') 
+  let play = `
+	≡ *FG MUSIC*
+┌──────────────
+▢ 📌 *${mssg.title}:* ${vid.title}
+▢ 📆 *${mssg.aploud}:* ${vid.ago}
+▢ ⌚ *${mssg.duration}:* ${vid.timestamp}
+▢ 👀 *${mssg.views}:* ${vid.views.toLocaleString()}
+└──────────────`
  await conn.sendButton2(m.chat, play, mssg.ig, thumbnail, [
-    ['🎶 MP3', `${usedPrefix}fgmp3 ${url}`],
-    ['🎥 MP4', `${usedPrefix}fgmp4 ${url}`]
+    ['Audio mp3', `${usedPrefix}ytmp3 ${url}`],
+    ['video mp4', `${usedPrefix}ytmp4 ${url}`]
   ], null, [['Canal', `${md}`]], m)
-		
-       try {
-       let yt = await fg.yta(vid.url, q)
-       let { title, dl_url, size } = yt
-       let limit = 100
-       
-if (size.split('MB')[0] >= limit) return conn.reply(m.chat,`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m).then(_ => m.react('✖️'))
-       
-await conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: "audio/mp4", fileName: vid.title + '.mp3', quoted: m, contextInfo: {
-'forwardingScore': 200,
-'isForwarded': true,
-externalAdReply:{
-showAdAttribution: false,
-title: `${vid.title}`,
-body: `${vid.author.name}`,
-mediaType: 2, 
-sourceUrl: `${vid.url}`,
-thumbnail: await (await fetch(vid.thumbnail)).buffer()}}}, { quoted: m })
-       await m.react('✅')
-       } catch {
-       try {
-       let yt = await fg.ytmp3(vid.url, q)
-       let { title, dl_url, size } = yt
-       let limit = 100
-       
-if (size.split('MB')[0] >= limit) return conn.reply(m.chat,`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m).then(_ => m.react('✖️'))
-       
-       await conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: "audio/mp4", fileName: vid.title + '.mp3', quoted: m, contextInfo: {
-'forwardingScore': 200,
-'isForwarded': true,
-externalAdReply:{
-showAdAttribution: false,
-title: `${vid.title}`,
-body: `${vid.author.name}`,
-mediaType: 2, 
-sourceUrl: `${vid.url}`,
-thumbnail: await (await fetch(vid.thumbnail)).buffer()}}}, { quoted: m })
-       await m.react('✅')
-       } catch (error) {
-        await conn.reply(m.chat,`*☓ Ocurrió un error inesperado*`, m, fake,).then(_ => m.react('✖️'))
-        console.error(error)
-    }}}
-        
-        if (feature == "mp4") {
-            if (!inputs) return conn.reply(m.chat, `*🚩 Ingresa el titulo de un video o musica de YouTube.*`, m, fake,)
-    await m.react('🕓')
-    let res = await yts(text)
-    let vid = res.videos[0]
-    let q = '360p'
-	let txt = `╭──────────✰\n`
-	   txt += `│🍭 *Título ∙* ${vid.title}\n`
-       txt += `│🕜 *Duración ∙* ${vid.timestamp}\n`
-       txt += `│👁 *Visitas ∙* ${vid.views}\n`
-       txt += `│📚 *Autor ∙* ${vid.author.name}\n`
-       txt += `│📅 *Publicado ∙* ${vid.ago}\n`
-       txt += `│⛓ *Url ∙* ${'https://youtu.be/' + vid.videoId}\n`
-       txt += `╰──────────✰\n\n`
-       txt += `*↻ Espera @${m.sender.split`@`[0]}, soy lenta. . .*`
-let buttonMessage= {
-'document': { url: `https://github.com/DanielDiod/DokiBot` },
-'mimetype': `application/pdf`,
-'fileName': `✰ | YᴏᴜTᴜʙᴇ Pʟᴀʏ 📥`,
-'fileLength': 99999999999999,
-'pageCount': 200,
-'contextInfo': {
-'mentionedJid': [m.sender],
-'forwardingScore': 200,
-'isForwarded': true,
-'externalAdReply': {
-'mediaUrl': `${vid.url}`,
-'mediaType': 2,
-'previewType': 'VIDEO',
-'title': `${vid.title}`,
-'body': null,
-'thumbnail': await (await fetch(vid.thumbnail)).buffer(),
-'sourceUrl': 'https://youtube.com/' }},
-'caption': txt,
-'footer': '\nVideos de YouTube',
-'headerType': 6 }
-conn.sendMessage(m.chat, buttonMessage, { quoted: m })
-       
-       try {
-       let yt = await fg.ytv(vid.url, q)
-       let { title, dl_url, size } = yt
-       let limit = 100
-       
-if (size.split('MB')[0] >= limit) return conn.reply(m.chat,`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`,  m, fake,).then(_ => m.react('✖️'))
-       
-       await conn.sendFile(m.chat, dl_url, 'yt.jpg', `${vid.title}\n⇆ㅤㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤ↻\n00:15 ━━━━●────── ${vid.timestamp}`, m)
-       await m.react('✅')
-       } catch {
-       try {
-       let yt = await fg.ytmp4(vid.url, q)
-       let { title, dl_url, size } = yt
-       let limit = 100
-       
-if (size.split('MB')[0] >= limit) return conn.reply(m.chat,`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, fake,).then(_ => m.react('✖️'))
-       
-       await conn.sendFile(m.chat, dl_url, 'yt.jpg', `${vid.title}\n⇆ㅤㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤ↻\n00:15 ━━━━●────── ${vid.timestamp}`, m)
-       await m.react('✅')
-       } catch (error) {
-        await conn.reply(m.chat,`*☓ Ocurrió un error inesperado*`, m).then(_ => m.react('✖️'))
-        console.error(error)
-}}}
-handler.help = ["play"].map(v => v + " <formato> <búsqueda>")
-handler.tags = ["downloader"]
-handler.command = ['play']
-handler.register = true 
-handler.star = 2
+}
+handler.help = ['play']
+handler.tags = ['dl']
+handler.command = ['play', 'playvid']
+handler.disabled = false
+
 export default handler
