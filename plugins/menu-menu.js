180e6e2133774023d6a5107bd842349a6ad52314
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-05-10 01:16:33 -0500

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -52,7 +52,7 @@ const defaultMenu = {
 
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   
-  await conn.sendButton(m.chat, wm, defaultMenu, thumbnail, [
+  await conn.sendButton(m.chat, wm, defaultMenu, before.thumbnail, [
 	['Creador', `${usedPrefix}creador`],
 	['tiempo', `${usedPrefix}uptime`]
   ], null, [['Canal', `${md}`]], m)
