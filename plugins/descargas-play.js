4a5c133dd5d3245cfe3cfb1a76e6ce3c46109013
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-12 18:23:13 -0500

    Actualizar descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -4,20 +4,21 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
   if (!text) throw `error intenta de nuevo`
 	let res = await yts(text)
 	let vid = res.videos[0]
-	if (!vid) throw `✳️ Vídeo/Audio no encontrado`
+	if (!vid) throw `🚩 *Lo que buscas no se encontró*`
 	let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid
 	//const url = 'https://www.youtube.com/watch?v=' + videoId
-	m.react('🎧') 
-  let play = `
-	≡ ${packname}
+  let text = `> ${wm}
 ┌──────────────
-▢ 📌 *Titulo:* ${vid.title}
-▢ ⌚ *duración:* ${vid.timestamp}
-▢ 👀 *vistas:* ${vid.views.toLocaleString()}
+💚 *Nombre:* 
+- ${vid.title}
+⏳️ *Tiempo:* 
+- ${vid.timestamp}
+👀 *Vistas:* 
+- ${vid.views.toLocaleString()}
 └──────────────`
- await conn.sendButton(m.chat,wm, play, res.videos[0].thumbnail, [
-    ['🍧 MP3', `${usedPrefix}fgmp3 ${url}`],
-    ['🍿 MP4', `${usedPrefix}fgmp4 ${url}`]
+ await conn.sendButton(m.chat, wm, text, res.videos[0].thumbnail, [
+    ['💎 𝗠𝗣3', `${usedPrefix}fgmp3 ${url}`],
+    ['📎 𝗠𝗣4', `${usedPrefix}fgmp4 ${url}`]
   ],
 }
 handler.help = ['botones']
