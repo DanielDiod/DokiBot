b9f73b79b1e75fca0f23e01b8da8ad1e35257154
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-12 14:05:19 -0500

    Actualizar descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -17,7 +17,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 ▢ ⌚ *duración:* ${vid.timestamp}
 ▢ 👀 *vistas:* ${vid.views.toLocaleString()}
 └──────────────`
- await conn.sendButton(m.chat, play, wm, thumbnail, [
+ await conn.sendButton(m.chat, play, fgig, thumbnail, [
     ['🍧 MP3', `${usedPrefix}fgmp3 ${url}`],
     ['🍿 MP4', `${usedPrefix}fgmp4 ${url}`]
   ],
