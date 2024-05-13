a860a77d916d28e8972af57c99fe6a5188f61383
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-13 10:23:49 -0500

    Actualizar rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -1,15 +1,10 @@
-import {createHash} from "crypto";
-let handler = async function (m, {conn, text, usedPrefix}) {
-  let sn = createHash("md5").update(m.sender).digest("hex");
-  m.reply(
-    `┏┅ ━━━━━━━━━━━━ ┅ ━
-┃ *Este es tu número de serie 👇🏻:* 
-┃ ${sn}
-┗┅ ━━━━━━━━━━━━ ┅ ━`.trim()
-  );
-};
-handler.help = ["myns"];
-handler.tags = ["xp"];
-handler.command = /^(myns|ceksn)$/i;
-handler.register = true;
-export default handler;
+import { createHash } from 'crypto'
+let handler = async function (m, { conn, text, usedPrefix }) {
+let sn = createHash('md5').update(m.sender).digest('hex')
+
+conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', '乂 N U M E R O - D E - S E R I E 乂', 'status@broadcast')
+}
+handler.help = ['myns']
+handler.tags = ['rg']
+handler.command = /^(myns|ceksn)$/i
+handler.register = true
