a5354bfd5294e295847d2f073876e9507e2cd8d7
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-21 10:16:07 -0500

    Actualizar propietario(a)-leavegc.js

--- plugins/propietario(a)-leavegc.js
+++ plugins/propietario(a)-leavegc.js
@@ -2,7 +2,7 @@ let handler = async (m, {conn, args, command}) => {
   await m.reply("_.🎌Adios a todos, Igna • Bot abandona el grupo_");
   await conn.groupLeave(m.chat);
 };
-handler.command = /^(salir|leavegc|leave|salirdelgrupo)$/i;
+handler.command = /^(salir|ignaadios|leave|salirdelgrupo)$/i;
 handler.group = true;
 handler.rowner = true;
 export default handler;
