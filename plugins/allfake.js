035d92af7978601c0a40d3ad3c6f9f9df8e6f740
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-28 08:51:09 -0500

    Actualizar allfake.js

--- plugins/allfake.js
+++ plugins/allfake.js
@@ -44,4 +44,7 @@ global.taguser = '@' + m.sender.split("@s.whatsapp.net")
 
 global.fake = { contextInfo:{ forwardingScore: 0, externalAdReply: {title: '👋 Hola ' + nombre, body: wm, sourceUrl: md, thumbnailUrl: imagen1 }}} //Fake simple
 
+global.fake = { contextInfo: { mentionedJid: conn.parseMention(wm), forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363263466636910@newsletter', serverMessageId: '', newsletterName: 'IGNA - BOT -MD' }, externalAdReply: { title: wm, body: `👋 Hola ` + nombre, mediaType: 1, renderLargerThumbnail: false, previewType: `PHOTO`, thumbnailUrl: imagen1, thumbnail: yoshiImg, sourceUrl: yt }}}, { quoted: m } //fake con reenviado de canal
+}
+
 export default handler
