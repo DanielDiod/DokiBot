d8e1affedba1fb36c01a16b9728e00ee567068a0
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-06-02 16:37:09 -0500

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -16,7 +16,7 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   teks += `└───────⭓
 
-*By Daniel  x naye <3*`;
+*By Daniel*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
