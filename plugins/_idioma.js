0580f23925a9bce7b517bcbea19c2c7fd8c7c9d7
Zam <141449338+AzamiJs@users.noreply.github.com>
2024-06-03 21:26:14 -0600

    Create _idioma.js

---
+++ plugins/_idioma.js
@@ -0,0 +1,18 @@
+let handler = async (m, { conn, isAdmin }) => {
+
+let _idioma = '🚩 *Seleccione su idioma*\n\n> Idiomas disponibles:\nes _(Español)_\nen _(Inglés)_\n\n`Ejemplo:`\n.idioma es'
+if (!text) {
+return conn.sendButton(m.chat, _idioma, '🍟 Seleccione su idioma', null, [['Español 🇲🇽', `.idioma es`], ['Inglés 🇺🇸', `.idioma en`]], null, null, m)
+}
+let choice = text.includes('es') ? 'es' : text.includes('en') ? 'en' : null
+if (!choice) {
+return m.reply('Solo puedes seleccionar español o inglés')
+}
+global.lenguajeGB = choice
+return m.reply(choice === 'es' ? 'Idioma configurado a Español 🇲🇽' : 'Language set to English 🇺🇲')
+
+}
+handler.command = /^lenguage|lenguaje|idioma$/i
+handler.rowner = true
+handler.botAdmin = true
+export default handler
