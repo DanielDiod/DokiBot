319826633b40d0d1506c8e767077ab06e1a43262
Zam <141449338+AzamiJs@users.noreply.github.com>
2024-06-03 21:35:46 -0600

    Update _idioma.js

--- plugins/_idioma.js
+++ plugins/_idioma.js
@@ -1,16 +1,22 @@
 let handler = async (m, { conn, text }) => {
 
-let _idioma = '🚩 *Seleccione su idioma*\n\n> Idiomas disponibles:\nes _(Español)_\nen _(Inglés)_\n\n`Ejemplo:`\n.idioma es'
+let _idioma = '🚩 *Seleccione su idioma*\n\n> Idiomas disponibles:\nes _(Español)_\nen _(Inglés)_\nid _(Indonesio)_\nar _(Árabe)_\npt _(Portugués)_\n\n`Ejemplo:`\n.idioma es'
 if (!text) {
-return conn.sendButton(m.chat, _idioma, '🍟 Seleccione su idioma', null, [['Español 🇲🇽', `.idioma es`], ['Inglés 🇺🇸', `.idioma en`]], null, null, m)
+return conn.sendButton(m.chat, _idioma, '🍟 Seleccione su idioma', null, [['Español 🇲🇽', `.idioma es`], ['Inglés 🇺🇸', `.idioma en`], ['Indonesio 🇮🇩', `.idioma id`]], null, null, m)
 }
-let choice = text.includes('es') ? 'es' : text.includes('en') ? 'en' : null
+let choice = text.includes('es') ? 'es' : text.includes('en') ? 'en' : text.includes('id') ? 'id' : text.includes('ar') ? 'ar' : text.includes('pt') ? 'pt' : null
 if (!choice) {
-return m.reply('Solo puedes seleccionar español o inglés')
+return m.reply('Por favor, seleccione uno de los idiomas disponibles: español, inglés o indonesio.')
 }
 global.lenguajeGB = choice
-return m.reply(choice === 'es' ? 'Idioma configurado a Español 🇲🇽' : 'Language set to English 🇺🇲')
-
+switch (choice) {
+case 'es':
+return m.reply('Idioma configurado a Español 🇲🇽')
+case 'en':
+return m.reply('Language set to English 🇺🇲')
+case 'id':
+return m.reply('Bahasa diatur ke Indonesia 🇮🇩')
+}
 }
 handler.command = /^lenguage|lenguaje|idioma$/i
 handler.rowner = true
