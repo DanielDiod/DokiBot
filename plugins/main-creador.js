c3130b78cbc695b867649b7a1427b3cfd5ccca7e
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-06-21 00:03:51 -0400

    Update main-creador.js

--- plugins/main-creador.js
+++ plugins/main-creador.js
@@ -1,9 +1,1 @@
-/* let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Daniel 🇦🇱\nFN:Daniel 🇦🇱\nORG:Daniel 🇦🇱\nTITLE:\nitem1.TEL;waid=51955918117:51955918117\nitem1.X-ABLabel:Daniel 🇦🇱\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Daniel 🇦🇱⁩\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'Daniel 🇦🇱', contacts: [{ vcard }] }}, {quoted: m})
-}
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['creadores', 'creator', 'creador', 'dueño'] 
-
-export default handler
+comando en falla xd By @Alba070503
