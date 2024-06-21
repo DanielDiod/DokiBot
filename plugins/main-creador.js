00e492a1ed0b349c91207dfced6d4f3eb58cd5e4
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-06-21 00:03:00 -0400

    Update main-creador.js

--- plugins/main-creador.js
+++ plugins/main-creador.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, usedPrefix, isOwner }) => {
+/* let handler = async (m, { conn, usedPrefix, isOwner }) => {
 let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Daniel 🇦🇱\nFN:Daniel 🇦🇱\nORG:Daniel 🇦🇱\nTITLE:\nitem1.TEL;waid=51955918117:51955918117\nitem1.X-ABLabel:Daniel 🇦🇱\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Daniel 🇦🇱⁩\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: 'Daniel 🇦🇱', contacts: [{ vcard }] }}, {quoted: m})
 }
